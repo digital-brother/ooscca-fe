327667dcf75c011a069bd411d94e75ffa14943fc
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-11-15 22:29:49 +0200

    Add schoolsStats mockup

--- src/app/homepage/clientSchools.js
+++ src/app/homepage/clientSchools.js
@@ -1,6 +1,5 @@
 import {Box} from "@mui/material";
 import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
 
 const SCHOOLS = [
   {src: "/milbourne-lodge.svg", label: "Milbourne Lodge "},
