97d406377825513b8815a9548e702643efc9106e
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-11-25 04:20:44 +0200

    Update header with IconButton

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,13 +1,26 @@
-import {Box} from "@mui/material";
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import {Box, IconButton} from "@mui/material";
 import Image from "next/image";
 
 export default function Header() {
   return (
     // TODO: p={1} - how much px?
-    <Box display="flex" justifyContent="space-between" p={3} sx={{borderBottom: 1, borderColor: "#CED4DA"}}>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      py={1}
+      pl={3}
+      pr={1.5}
+      // TODO: Fix color
+      sx={{borderBottom: 1, borderColor: "#CED4DA"}}
+    >
       <Image src="/logo.png" width={117} height={27} alt="OOSCCA logo"/>
       {/* TODO: Add menu */}
-      <Image src="/hamburger.svg" width={36} height={21} alt="Menu icon"/>
+      <IconButton>
+        {/*TODO: Fix color*/}
+        <MenuOutlinedIcon sx={{fontSize: 33, color: "#0C0E0F"}}/>
+      </IconButton>
     </Box>
   )
 }
