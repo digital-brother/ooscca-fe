65189e9a3970f649ee838450bcbc16a723505d09
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-11-25 03:26:44 +0200

    Add HamburgerMenu.js template

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,11 +1,10 @@
-// TODO: Correct file name Header? (it is a component)
 import {Box} from "@mui/material";
 import Image from "next/image";
 
 export default function Header() {
   return (
     // TODO: p={1} - how much px?
-    <Box display="flex" justifyContent="space-between" p={2} sx={{borderBottom: 1}}>
+    <Box display="flex" justifyContent="space-between" p={3} sx={{borderBottom: 1, borderColor: "#CED4DA"}}>
       <Image src="/logo.png" width={117} height={27} alt="OOSCCA logo"/>
       {/* TODO: Add menu */}
       <Image src="/hamburger.svg" width={36} height={21} alt="Menu icon"/>
