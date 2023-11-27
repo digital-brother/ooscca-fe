e7b61b42d1c892dedbc1a277404760ed8bae9536
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-11-27 19:50:53 +0200

    Update ClientSchools.js with background

--- src/app/(homepage)/ClientSchools.js
+++ src/app/(homepage)/ClientSchools.js
@@ -1,6 +1,6 @@
 'use client'
 
-import {Box} from "@mui/material";
+import {Box, Container} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 const SCHOOLS = [
@@ -14,27 +14,29 @@ const SCHOOLS = [
 
 export default function ClientSchools() {
   return (
-    <Box mt={{xs: 5, md: 10}}>
-      <Typography variant="body1" textAlign="center" fontWeight="bold" color="text.secondary">
-        Join other parents from local schools who already use OOSCCA
-      </Typography>
+    <Box sx={{bgcolor: "#F8F9FA"}}>
+      <Container sx={{py: {xs: 6, md: 10}}}>
+        <Typography variant="body1" textAlign="center" fontWeight="bold" color="text.secondary">
+          Join other parents from local schools who already use OOSCCA
+        </Typography>
 
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: {
-            xs: "repeat(2, 150px)",
-            sm: "repeat(3, 150px)",
-            lg: "repeat(6, 160px)"
-          },
-          gap: 2,
-          justifyContent: "center",
-          mt: 5,
-        }}
-      >
-        {SCHOOLS.map((school, key) => <School key={key}  {...school}/>)}
-      </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: {
+              xs: "repeat(2, 150px)",
+              sm: "repeat(3, 150px)",
+              lg: "repeat(6, 160px)"
+            },
+            gap: 2,
+            justifyContent: "center",
+            mt: 5,
+          }}
+        >
+          {SCHOOLS.map((school, key) => <School key={key}  {...school}/>)}
+        </Box>
 
+      </Container>
     </Box>
   )
 }
