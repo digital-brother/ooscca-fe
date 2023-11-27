d6390107ef4d496d18eb15eaa94b509f516b4692
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-11-27 21:58:30 +0200

    Add benefits section container

--- src/app/(homepage)/Benefits.js
+++ src/app/(homepage)/Benefits.js
@@ -1,59 +1,61 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import {Paper} from "@mui/material";
+import {Box, Container} from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const BENEFITS = [
   {
     icon: "/hat.svg",
-    title: "Simplify Your Life",
-    description: "Keep your family's activity schedule organised effortlessly. With OOSCCA, track your bookings, manage schedules, and coordinate with other parents smoothly, all from one convenient location."
+    subheader: "Parenting is hard enough",
+    header: "Multiple bookings. One Seamless Experience",
+    description: "Whether it's a dance class on Monday, a tennis session on Wednesday, or a craft club on Friday - book them all through OOSCCA. Plan a multi-activity week for your child with just a single, seamless payment. Countless providers, yet the ease of one transaction. Your child's diverse week of fun is now uncomplicated to put together."
   },
   {
     icon: "/hat.svg",
-    title: "Maximise Family Time",
-    description: "Let OOSCCA do the legwork by bringing together a vast selection of clubs and activities in one place. Spend less time searching and more time enjoying life's precious moments with your children."
+    subheader: "Parenting is hard enough",
+    header: "Multiple bookings. One Seamless Experience",
+    description: "Whether it's a dance class on Monday, a tennis session on Wednesday, or a craft club on Friday - book them all through OOSCCA. Plan a multi-activity week for your child with just a single, seamless payment. Countless providers, yet the ease of one transaction. Your child's diverse week of fun is now uncomplicated to put together."
   },
   {
     icon: "/hat.svg",
-    title: "Maximise Family Time",
-    description: "Let OOSCCA do the legwork by bringing together a vast selection of clubs and activities in one place. Spend less time searching and more time enjoying life's precious moments with your children."
+    subheader: "Parenting is hard enough",
+    header: "Multiple bookings. One Seamless Experience",
+    description: "Whether it's a dance class on Monday, a tennis session on Wednesday, or a craft club on Friday - book them all through OOSCCA. Plan a multi-activity week for your child with just a single, seamless payment. Countless providers, yet the ease of one transaction. Your child's diverse week of fun is now uncomplicated to put together."
   },
 ]
+
+function BenefitText({benefit}) {
+  return (
+    <Box>
+      <Typography variant="body1" color="warning.main" fontWeight="bold">
+        {benefit.subheader}
+      </Typography>
+      <Typography mt={2} variant="h3" fontWeight="bold">
+        {benefit.header}
+      </Typography>
+      <Typography mt={2} variant="body1" color="text.secondary">
+        {benefit.description}
+      </Typography>
+    </Box>
+  )
+}
 
 export default function Benefits() {
   return (
     <>
-      <Box sx={{textAlign: "center", mt: {xs: 5, md: 10}, mx: "auto", maxWidth: 600}}>
-        <Typography variant="body2" textAlign="center" fontWeight="bold" color="warning.main">
-          It’s easy to get started. And free.
-        </Typography>
-        <Typography variant="h4" fontWeight="bold" mt={1}>
-          Platform that puts you in control again
-        </Typography>
-        <Typography variant="body1" color="text.secondary" mt={2}>
-          The activity planning reimagined the way parents want to arrange,
-          pay and manage activities with multiple providers.
-        </Typography>
-      </Box>
-
-      <Box sx={{display: 'flex', justifyContent: 'center'}}>
-        <Grid container justifyContent="center" alignItems="center" spacing={5}
-              sx={{textAlign: "center", pt: {xs: 5, md: 10}, maxWidth: {xs: 500, md: 1200}}}
-        >
+      <Container sx={{py: {xs: 6, md: 10}}}>
+        <Grid container rowSpacing={{md: 12}}>
           {BENEFITS.map(benefit => (
-            <Grid item xs={12} md={4}>
-              <Paper variant="outlined" sx={{p: 3}}>
-                <img src={benefit.icon} />
-                <Typography variant="h6" fontWeight="bold" color="text.secondary" mt={1}>
-                  {benefit.title}
-                </Typography>
-                <Typography variant="body1" color="text.secondary">{benefit.description}</Typography>
-              </Paper>
-            </Grid>
+            <>
+              <Grid item xs={12} md={6}>
+                <BenefitText benefit={benefit}/>
+              </Grid>
+              <Grid item>
+                <Box component="img" src="/benefit.png" width="100%"/>
+              </Grid>
+            </>
           ))}
         </Grid>
-      </Box>
+      </Container>
     </>
   )
 }
