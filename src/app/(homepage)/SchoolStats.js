08704bab8649c379d01ed5960e8c8cc2b706b811
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-02 02:38:03 +0200

    Add layout

--- src/app/(homepage)/SchoolStats.js
+++ src/app/(homepage)/SchoolStats.js
@@ -1,9 +1,23 @@
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 export default function SchoolStats() {
   return (
     <Box>
-      School Stats
+      <Grid container>
+        <Grid item xs={12} md={6} sx={{ order: { xs: 1, md: 3 } }}>
+          School 1
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ order: { xs: 2, md: 4 } }}>
+          School 2
+        </Grid>
+        <Grid item xs={6} sx={{ order: { xs: 3, md: 1 } }}>
+          Label 1
+        </Grid>
+        <Grid item xs={6} sx={{ order: { xs: 4, md: 2 } }}>
+          Label 2
+        </Grid>
+      </Grid>
     </Box>
-  )
+  );
 }
