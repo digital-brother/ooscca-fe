b0126dfc3aaeaf7c52140915d114e85211c51b88
Slava Loievskyi <loievskyi.slava@gmail.com>
2023-12-08 20:24:53 +0200

    calendar now switches values (slowly)

--- src/app/(homepage)/Calendar.js
+++ src/app/(homepage)/Calendar.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
-import { Paper, Typography } from '@mui/material';
+import { Paper, Typography, Box } from '@mui/material';
 import dayjs from 'dayjs';
 
 import 'slick-carousel/slick/slick.css';
@@ -11,124 +11,190 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 const Calendar = () => {
+  // from backend
   const months = [
-    ["January", "2023-01-01",],
-    ["February", "2023-02-01",],
-    ["March", "2023-03-01",],
-    ["April", "2023-04-01",],
-    ["May", "2023-05-01",],
-    ["June", "2023-06-01",],
-    ["July", "2023-07-01",],
-    ["August", "2023-08-01",],
-    ["September", "2023-09-01",],
-    ["October", "2023-10-01",],
-    ["November", "2023-11-01",],
-    ["December", "2023-12-01",],
-  ]
+    "2023-01-01",
+    "2023-02-12",
+    "2023-03-01",
+    "2023-04-12",
+    "2023-05-23",
+    "2023-06-01",
+    "2023-07-25",
+    "2023-08-25",
+    "2023-09-01",
+    "2023-10-01",
+    "2023-11-01",
+    "2023-12-01",
+    "2024-01-01",
+    "2024-02-01",
+    "2024-03-01",
+    "2024-04-01",
+    "2024-05-01",
+    "2024-06-01",
+    "2024-07-01",
+  ];
+  const [sliceValues, setSliceValues] = useState([0, 4]);
 
   const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    function nextOnClick() {
+      if (sliceValues[1] < months.length) {
+        setSliceValues(sliceValues => [sliceValues[0]+1, sliceValues[1]+1])
+        setRenderedMonths(renderedMonths => [
+          ...renderedMonths.slice(1),
+          (<CustomDataCalendar date={months[sliceValues[1]]} />)
+        ]);
+      }
+    }
+    return (
+      <Box
+        className={className}
+        style={{ ...style, display: "block", background: "black" }}
+        onClick={nextOnClick}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    function prevOnClick() {
+      if (sliceValues[0] > 0) {
+        setSliceValues(sliceValues => [sliceValues[0]-1, sliceValues[1]-1])
+        setRenderedMonths(renderedMonths => [
+          (<CustomDataCalendar date={months[sliceValues[0]-1]} />),
+          ...renderedMonths.slice(0, -1),
+        ]);
+      }
+    }
+    return (
+      <Box
+        className={className}
+        style={{ ...style, display: "block", background: "black" }}
+        onClick={prevOnClick}
+      />
+    );
+  }
+
+  function CustomDataCalendar({date}) {
+    return (<DateCalendar
+      onChange={(newValue) => setValue(newValue)}
+      value={dayjs(date)}
+      minDate={dayjs(date).startOf("month")}
+      maxDate={dayjs(date).endOf("month")}
+      dayOfWeekFormatter={(_day, weekday) => `${weekday.format("dd")}`}
+      views={['day']}
+      disableHighlightToday
+      disabled
+      sx={{
+        width: "350px",
+        minHeight: "400px",
+
+        '.MuiPickersCalendarHeader-root': {
+          justifyContent: "center",
+          m: 0,
+          mb: "15px",
+
+          color: "var(--light-primary, var(--bg-black-80, #333))",
+          fontFamily: "Inter",
+          fontSize: "14.752px",
+          fontStyle: "normal",
+          fontWeight: "700",
+          lineHeight: "normal",
+
+        },
+        ".MuiPickersCalendarHeader-labelContainer": {
+          justifyContent: "center",
+          m: 0,
+          color: "var(--light-primary, var(--bg-black-80, #333))",
+          fontFamily: "Inter, inherit",
+          fontSize: "14.752px",
+          fontStyle: "normal",
+          fontWeight: "700",
+          lineHeight: "normal",
+        },
+        ".MuiDayCalendar-weekDayLabel ": {
+          color: "var(--light-primary, var(--bg-black-80, #333))",
+          textAlign: "center",
+          fontFamily: "Inter, inherit",
+          fontSize: "14.752px",
+          fontStyle: "normal",
+          fontWeight: "500",
+          lineHeight: "normal",
+          height: "45px",
+          width: "45px",
+        },
+        ".MuiPickersArrowSwitcher-root ": { // buttons < > on header
+          display: "none",
+        },
+        ".MuiPickersDay-root ": {
+          opacity: "100%",
+          color: '#1565c0',
+          borderRadius: 2,
+          borderWidth: 1,
+          color: "#666",
+          textAlign: "center",
+          fontFamily: "Inter, inherit",
+          fontSize: "14.752px",
+          fontStyle: "normal",
+          fontWeight: "500",
+          lineHeight: "normal",
+          height: "45px",
+          width: "45px",
+        },
+        ".MuiPickersDay-root.Mui-selected": {
+          opacity: "100%",
+          borderRadius: "6px",
+          backgroundColor: "#FFC50A",
+          flexShrink: 0,
+
+          color: "var(--primary-black, #0C0E0F)",
+          textAlign: "center",
+          fontFamily: "Inter, inherit",
+          fontSize: "14.752px",
+          fontStyle: "normal",
+          fontWeight: "700",
+          lineHeight: "normal",
+          height: "45px",
+          width: "45px",
+        },
+        ".MuiPickersDay-root.Mui-selected:hover": {
+          backgroundColor: "00A8DE",
+          opacity: "100%",
+        },
+        ".MuiPickersSlideTransition-root": {
+          height: "300px",
+          opacity: "100%",
+        }
+      }}
+    />);
+  };
+
+  const [renderedMonths, setRenderedMonths] = useState(
+    months.slice(sliceValues[0], sliceValues[1]).map((month, index) => (
+      <CustomDataCalendar date={month} />
+    ))
+  );
 
   return (
     <Slider {...settings}>
-      {months.map((month, index) => (
-        <div key={index}>
+      {renderedMonths.map((month, index) => (
+        <Box key={index}>
           <Paper style={{ padding: '20px', textAlign: 'center' }}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DateCalendar
-                defaultValue={dayjs(month[1])}
-                minDate={dayjs(month[1]).startOf("month")}
-                maxDate={dayjs(month[1]).endOf("month")}
-                dayOfWeekFormatter={(_day, weekday) => `${weekday.format("dd")}`}
-                views={['day']}
-                sx={{
-                  width: "350px",
-                  // height: "400px",
-                  minHeight: "400px",
-
-                  '.MuiPickersCalendarHeader-root': {
-                    justifyContent: "center",
-                    m: 0,
-                    mb: "15px",
-
-                    color: "var(--light-primary, var(--bg-black-80, #333))",
-                    fontFamily: "Inter",
-                    fontSize: "14.752px",
-                    fontStyle: "normal",
-                    fontWeight: "700",
-                    lineHeight: "normal",
-
-                  },
-                  ".MuiPickersCalendarHeader-labelContainer": {
-                    justifyContent: "center",
-                    m: 0,
-                    color: "var(--light-primary, var(--bg-black-80, #333))",
-                    fontFamily: "Inter, inherit",
-                    fontSize: "14.752px",
-                    fontStyle: "normal",
-                    fontWeight: "700",
-                    lineHeight: "normal",
-                  },
-                  ".MuiDayCalendar-weekDayLabel": {
-                    color: "var(--light-primary, var(--bg-black-80, #333))",
-                    textAlign: "center",
-                    fontFamily: "Inter, inherit",
-                    fontSize: "14.752px",
-                    fontStyle: "normal",
-                    fontWeight: "500",
-                    lineHeight: "normal",
-                    height: "45px",
-                    width: "45px",
-                  },
-                  ".MuiPickersArrowSwitcher-root": { // buttons < > on header
-                    display: "none",
-                  },
-                  ".MuiPickersDay-root": {
-                    color: '#1565c0',
-                    borderRadius: 2,
-                    borderWidth: 1,
-                    color: "#666",
-                    textAlign: "center",
-                    fontFamily: "Inter, inherit",
-                    fontSize: "14.752px",
-                    fontStyle: "normal",
-                    fontWeight: "500",
-                    lineHeight: "normal",
-                    height: "45px",
-                    width: "45px",
-                  },
-                  ".MuiPickersDay-root.Mui-selected": {
-                    borderRadius: "6px",
-                    backgroundColor: "#FFC50A",
-                    flexShrink: 0,
-
-                    color: "var(--primary-black, #0C0E0F)",
-                    textAlign: "center",
-                    fontFamily: "Inter, inherit",
-                    fontSize: "14.752px",
-                    fontStyle: "normal",
-                    fontWeight: "700",
-                    lineHeight: "normal",
-                    height: "45px",
-                    width: "45px",
-                  },
-                  ".MuiPickersDay-root.Mui-selected:hover": {
-                    backgroundColor: "00A8DE",
-                    opacity: "100%",
-                  },
-                  ".MuiPickersSlideTransition-root": {
-                    height: "300px",
-                  }
-                }}
-              />
-
+              {month}
             </LocalizationProvider>
           </Paper>
-        </div>
+        </Box>
       ))}
     </Slider>
   );
