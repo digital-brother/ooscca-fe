7ee5d9e088a8f1d3fced929f51aa76d5fdc8c946
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-11 21:23:27 +0200

    Make activity first step layout

--- src/app/activities/page.js
+++ src/app/activities/page.js
@@ -1,9 +1,32 @@
+"use client"
+
 import Box from "@mui/material/Box";
+import {styled} from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+const ActivitiesBox = styled(Box)({
+  border: "1px solid #6C757D",
+  borderRadius: 16,
+  width: 541,
+  height: 597,
+})
+
+function ActivitiesFirst() {
+  return (
+    <ActivitiesBox sx={{display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center"}}>
+      <Box sx={{maxWidth: 341}}>
+        <Typography variant="h5">
+          Create your first activity and let’s get going
+        </Typography>
+      </Box>
+    </ActivitiesBox>
+  )
+}
 
 export default function Activities() {
   return (
-    <Box>
-      Activities
+    <Box sx={{m: 10}}>
+      <ActivitiesFirst/>
     </Box>
   )
 }
