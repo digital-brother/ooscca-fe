c51ef4193c000be9be4afc51a375a3f743328cbd
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-11 21:47:10 +0200

    Wrap first activity into carousel

--- src/app/activities/page.js
+++ src/app/activities/page.js
@@ -3,13 +3,14 @@
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import {Select} from "@/app/components/Select";
+import { Select } from "@/app/components/Select";
+import Carousel from "@/app/activities/components/Carousel";
 
 const ACTIVITIES_TYPES = [
-  {id: 1, name: "Football"},
-  {id: 2, name: "Basketball"},
-  {id: 2, name: "Swimming"},
-]
+  { id: 1, name: "Football" },
+  { id: 2, name: "Basketball" },
+  { id: 2, name: "Swimming" },
+];
 
 const ActivitiesBox = styled(Box)({
   border: "1px solid #6C757D",
@@ -32,7 +33,11 @@ function ActivitiesFirst() {
         <Typography variant="h5">
           Create your first activity and let’s get going
         </Typography>
-        <Select label="Pick activity from list" items={ACTIVITIES_TYPES} sx={{mt: 4}} />
+        <Select
+          label="Pick activity from list"
+          items={ACTIVITIES_TYPES}
+          sx={{ mt: 4 }}
+        />
       </Box>
     </ActivitiesBox>
   );
@@ -41,7 +46,9 @@ function ActivitiesFirst() {
 export default function Activities() {
   return (
     <Box sx={{ m: 10 }}>
-      <ActivitiesFirst />
+      <Carousel>
+        <ActivitiesFirst />
+      </Carousel>
     </Box>
   );
 }
