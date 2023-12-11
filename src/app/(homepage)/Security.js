81e9f7fa40121a6617d56f10af52acdfe85d1ad4
NikGorbunov <gorbunovmya.@gmail.com>
2023-12-11 08:29:35 +0200

    made changes after review

--- src/app/(homepage)/Security.js
+++ src/app/(homepage)/Security.js
@@ -5,131 +5,54 @@ import {Manrope, Montserrat} from "@next/font/google";
 const manrope = Manrope({subsets: ['latin']});
 const montserrat = Montserrat({subsets: ['latin']});
 
-const securityInlineStyles = {
-    textAlign: "center",
-    maxWidth: "638px",
-    '@media (max-width: 900px)': {
-        textAlign: "center",
-        maxWidth: "358px",
-    }
-};
-
-const inlineStyles = {
-    textAlign: "center",
-    fontFamily: montserrat.style,
-    fontSize: "16px",
-    fontStyle: "normal",
-    fontWeight: "700",
-    lineHeight: "24px",
-    letterSpacing: "0.16px",
-    color: "#FF8919",
-    '@media (max-width: 900px)': {
-        fontSize: "16px",
-        fontStyle: "normal",
-        fontWeight: 700,
-        lineHeight: "24px",
-        letterSpacing: "0.16px",
-        fontFamily: montserrat.style,
-        color: "#FF8919",
-    },
-}
-
-const titleStyles = {
-    fontSize: "48px",
-    fontStyle: "normal",
-    fontWeight: 700,
-    lineHeight: "56px",
-    letterSpacing: "-0.72px",
-    color: "#FFF",
-    fontFamily: montserrat.style,
-    '@media (max-width: 900px)': {
-        fontSize: "32px",
-        fontStyle: "normal",
-        fontWeight: 700,
-        lineHeight: "44px",
-        letterSpacing: "0.16px",
-        fontFamily: montserrat.style,
-        color: "#FFF",
-    },
-};
-
-const contentStyles = {
-    fontSize: "24px",
-    fontStyle: "normal",
-    fontWeight: 400,
-    lineHeight: "32px",
-    color: "#FFF",
-    fontFamily: manrope.style,
-    '@media (max-width: 900px)': {
-        fontSize: "16px",
-        fontStyle: "normal",
-        fontWeight: 400,
-        lineHeight: "24px",
-        fontFamily: manrope.style,
-        color: "#FFF",
-        marginBottom: "73px"
-    },
-};
-
-const imageSecurity3Styles = {
-    width: "327px",
-    height: "239px",
-    overflow: "hidden",
-    '@media (max-width: 900px)': {
-        width: "175px",
-        height: "127px",
-    }
-
-}
-
-const imageSecurity4Styles = {
-    width: "376px",
-    height: "221px",
-    overflow: "hidden",
-    '@media (max-width: 900px)': {
-        width: "298px",
-        height: "175px",
-        marginRight: "50px",
-        position: "relative",
-        bottom: "55px",
-    }
-
-}
-
-const containerInlineSlytes = {
-    display: "flex",
-    background: "#0C0E0F",
-    py: {
-        xs: 6,
-        md: 10,
-    },
-    px: {
-        sm: 0,
-        md: 0,
-    },
-    mx: {
-        sm: 0,
-        md: 0,
-    },
-    '@media (max-width: 900px)': {
-        maxWidth: "900px",
-        display: "flex",
-        flexDirection: "column-reverse",
-        alignItems: "center",
-        paddingTop: "0px",
-    },
-}
-
-function SecurityHeader({sx}) {
+function SecurityHeader() {
     return (
-        <Box sx={{...securityInlineStyles, ...sx}}>
-            <Typography variant="body2" className={montserrat.className} sx={inlineStyles}>
+        <Box sx={{
+            textAlign: "center",
+            mx: {
+                xs: "auto",
+                md: "auto",
+            },
+            maxWidth: {
+                xs: "358px",
+                md: "638px",
+            }
+        }}>
+            <Typography variant="body2" className={montserrat.className} sx={{
+                textAlign: "center",
+                fontFamily: montserrat.style,
+                fontSize: "16px",
+                fontStyle: "normal",
+                fontWeight: "700",
+                lineHeight: "24px",
+                letterSpacing: "0.16px",
+                color: "#FF8919",
+            }}>
                 Your Trust, Our Commitment
             </Typography>
-            <Typography mt={2} variant="h3" sx={titleStyles} className={montserrat.className}>
+            <Typography mt={2} variant="h3" sx={{
+                color: "#FFF",
+                fontFamily: montserrat.style,
+                fontStyle: "normal",
+                fontWeight: 700,
+                fontSize: {md: "48px", xs: "32px"},
+                lineHeight: {md: "56px", xs: "44px"},
+                letterSpacing: {md: "-0.72px", xs: "0.16px"},
+
+            }} className={montserrat.className}>
                 Uncompromised Security
             </Typography>
-            <Typography mt={3} variant="body1" sx={contentStyles} className={manrope.className}>
+            <Typography mt={3} variant="body1" sx={{
+                color: "#FFF",
+                fontFamily: manrope.style,
+                fontStyle: "normal",
+                fontWeight: 400,
+                fontSize: {md: "24px", xs: "16px"},
+                lineHeight: {md: "32px", xs: "24px"},
+                mb: {
+                    xs: "72px"
+                }
+            }} className={manrope.className}>
                 At OOSCCA, we understand that security isn't just a feature; it's a foundation. In a digital world
                 where your family's safety and privacy are paramount, we are steadfast in our commitment to
                 protecting your information with the utmost rigor.
@@ -141,14 +64,50 @@ function SecurityHeader({sx}) {
 export default function Security() {
     return (
         <Container
-            sx={containerInlineSlytes} style={{
-            maxWidth: "1441px",
-            alignItems: "center",
-        }}
+            sx={{
+                display: "flex",
+                background: "#0C0E0F",
+                alignItems: "center",
+                py: {
+                    xs: 6,
+                    md: 10,
+                },
+                px: {
+                    sm: 0,
+                    md: 0,
+                },
+                mx: {
+                    sm: 0,
+                    md: 0,
+                    lg: "auto",
+                    xl: "auto",
+                },
+                maxWidth: {md: "1441px", xs: "900px"},
+                flexDirection: {md: "initial", xs: "column-reverse"},
+            }}
         >
-            <Box component="img" src="/security3.png" sx={imageSecurity3Styles}/>
-            <SecurityHeader sx={{mx: "auto"}}/>
-            <Box component="img" src="/security4.png" sx={imageSecurity4Styles}/>
+            <Box component="img" sx={{
+                width: {md: "327px", xs: "175px"},
+                height: {md: "264px", xs: "127px"},
+                backgroundImage: {md: `url('/security3.png')`, xs: `url('/security3xs.png')`},
+                backgroundSize: "cover",
+
+            }
+            }/>
+            <SecurityHeader/>
+            <Box component="img" sx={{
+                width: {md: "376px", xs: "298px"},
+                height: {md: "241px", xs: "175px"},
+                marginRight: {
+                    md: "0px",
+                    xs: "50px",
+                },
+                position: {xs: "relative"},
+                bottom: {md: "0px", xs: "103px"},
+                backgroundImage: {md: `url('/security4.png')`, xs: `url('/security4xs.png')`},
+                backgroundSize: "cover",
+
+            }}/>
         </Container>
     );
 }
