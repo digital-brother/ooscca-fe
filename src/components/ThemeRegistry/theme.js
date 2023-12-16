cdea9df503b14f2724dd0cd062671c58c9db5ad4
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-16 15:24:33 +0200

    Format theme.js

--- src/components/ThemeRegistry/theme.js
+++ src/components/ThemeRegistry/theme.js
@@ -1,54 +1,54 @@
-import {createTheme} from '@mui/material/styles';
-import {Libre_Franklin, Manrope} from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+import { Libre_Franklin, Manrope } from "next/font/google";
 
 const libreFranklin = Libre_Franklin({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const manrope = Manrope({
-  weight: ['600'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["600"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const baseTheme = createTheme();
 
 const theme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "light",
   },
   typography: {
     fontFamily: libreFranklin.style.fontFamily,
     h1: {
-      fontSize: '3rem', // 48px
+      fontSize: "3rem", // 48px
       fontWeight: 700,
     },
     h2: {
-      fontSize: '2.5rem', // 40px
+      fontSize: "2.5rem", // 40px
       fontWeight: 700,
     },
     h3: {
-      fontSize: '2rem', // 32px
+      fontSize: "2rem", // 32px
       fontWeight: 700,
     },
     h5: {
-      fontSize: '1.5rem', //24
+      fontSize: "1.5rem", //24
       fontWeight: 700,
     },
     subheading: {
-      fontSize: '1rem', // 16px
+      fontSize: "1rem", // 16px
       fontWeight: 700,
       color: baseTheme.palette.warning.main,
-    }
+    },
   },
   components: {
     MuiAlert: {
       styleOverrides: {
-        root: ({ownerState}) => ({
-          ...(ownerState.severity === 'info' && {
-            backgroundColor: '#60a5fa',
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
           }),
         }),
       },
