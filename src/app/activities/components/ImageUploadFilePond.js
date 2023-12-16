eaee4289a21c6bce5f38f478f4a85f76bdbe04a3
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-16 22:30:36 +0200

    Filepond source

--- src/app/activities/components/ImageUploadFilePond.js
+++ src/app/activities/components/ImageUploadFilePond.js
@@ -1,28 +1,39 @@
-"use client";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-import React, { useState } from "react";
-import { FilePond, registerPlugin } from "react-filepond";
+// Import React FilePond
+import { FilePond, registerPlugin } from 'react-filepond';
 
-import "filepond/dist/filepond.min.css";
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
 
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
+// Register the plugins
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
-export default function ImageUploadFilePond() {
-  const [files, setFiles] = useState([]);
-  return (
-    <FilePond
-      files={files}
-      onupdatefiles={setFiles}
-      allowMultiple={true}
-      maxFiles={3}
-      server="/api"
-      name="files"
-      labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
-      credits={false}
-    />
-  );
+// Our app
+function App() {
+    const [files, setFiles] = useState([]);
+    return (
+        <div className="App">
+            <FilePond
+                files={files}
+                onupdatefiles={setFiles}
+                allowMultiple={true}
+                maxFiles={3}
+                server="/api"
+                name="files"
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+            />
+        </div>
+    );
 }
+
+// Attach App to DOM
+ReactDOM.render(<App />, document.getElementById('root'));
