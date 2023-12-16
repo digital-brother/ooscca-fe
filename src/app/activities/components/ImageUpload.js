4abd1b82ab03a65e53a766eed6e20b758378f82c
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-16 04:47:22 +0200

    Extract ImagePreview

--- src/app/activities/components/ImageUpload.js
+++ src/app/activities/components/ImageUpload.js
@@ -3,19 +3,19 @@
 import { useRef, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 
-function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
-}
-
-export default function ImageUpload() {
-  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
+function ImageInput({ setImagePreviewUrl }) {
   const inputFileRef = useRef(null);
+
+  const getBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  };
 
   const handleFileChange = async (event) => {
     const file = event.target.files[0];
@@ -24,6 +24,61 @@ export default function ImageUpload() {
       setImagePreviewUrl(dataURL);
     }
   };
+
+  return (
+    <Box
+      sx={{
+        bgcolor: "#DEE2E6",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 1,
+      }}
+    >
+      <Typography sx={{ fontWeight: 700 }}>
+        Drop your logo file here or &nbsp;
+        <span
+          onClick={() => inputFileRef.current.click()}
+          style={{ cursor: "pointer", color: "#B01FB8" }}
+        >
+          browse
+        </span>
+      </Typography>
+
+      <Typography variant="caption">
+        Max. file size: 5MB &nbsp;&nbsp;&nbsp;&nbsp; Dimension: 000 x 000px
+      </Typography>
+
+      <input
+        type="file"
+        id="avatar"
+        name="avatar"
+        accept="image/png, image/jpeg"
+        onChange={handleFileChange}
+        ref={inputFileRef}
+        hidden
+      />
+    </Box>
+  );
+}
+
+function ImagePreview({ imagePreviewUrl }) {
+  return (
+    <Box sx={{ position: "relative", width: "100%", height: "100%" }}>
+      <Box
+        component="img"
+        src={imagePreviewUrl}
+        alt="Preview"
+        sx={{ objectFit: "contain", width: "100%", height: "100%" }}
+      />
+    </Box>
+  );
+}
+
+export default function ImageUpload() {
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
 
   return (
     <>
@@ -36,50 +91,12 @@ export default function ImageUpload() {
           overflow: "hidden",
         }}
       >
-        <Box
-          sx={{
-            bgcolor: "#DEE2E6",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: 1,
-          }}
-        >
-          <Typography sx={{ fontWeight: 700 }}>
-            Drop your logo file here or &nbsp;
-            <span
-              onClick={() => inputFileRef.current.click()}
-              style={{ cursor: "pointer", color: "#B01FB8" }}
-            >
-              browse
-            </span>
-          </Typography>
-
-          <Typography variant="caption">
-            Max. file size: 5MB &nbsp;&nbsp;&nbsp;&nbsp; Dimension: 000 x 000px
-          </Typography>
-
-          <input
-            type="file"
-            id="avatar"
-            name="avatar"
-            accept="image/png, image/jpeg"
-            onChange={handleFileChange}
-            ref={inputFileRef}
-            hidden
-          />
-        </Box>
+        {imagePreviewUrl ? (
+          <ImagePreview imagePreviewUrl={imagePreviewUrl} />
+        ) : (
+          <ImageInput setImagePreviewUrl={setImagePreviewUrl} />
+        )}
       </Box>
-
-      {imagePreviewUrl && (
-        <img
-          src={imagePreviewUrl}
-          alt="Preview"
-          style={{ marginTop: "20px", maxHeight: "200px" }}
-        />
-      )}
     </>
   );
 }
