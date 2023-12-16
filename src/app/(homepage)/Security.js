0f9116e2152321c6f29158d9efc83da68b9a981a
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-16 16:29:26 +0200

    Reorder images

--- src/app/(homepage)/Security.js
+++ src/app/(homepage)/Security.js
@@ -3,15 +3,17 @@
 import { Box, Container, Typography } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
-import { manrope, montserrat } from "@/components/ThemeRegistry/theme";
 
 function SecurityHeader() {
   const theme = useTheme();
   const smUp = useMediaQuery(theme.breakpoints.up("sm"));
   return (
-    <Box sx={{textAlign: "center"}}>
+    <Container sx={{ textAlign: "center" }}>
       <Typography variant="subheading">Your Trust, Our Commitment</Typography>
-      <Typography variant={smUp ? "h1" : "h3"} sx={{mt: 2, color: "#FFF", letterSpacing: -1 }}>
+      <Typography
+        variant={smUp ? "h1" : "h3"}
+        sx={{ mt: 2, color: "#FFF", letterSpacing: -1 }}
+      >
         Uncompromised Security
       </Typography>
       <Typography sx={{ mt: 3, fontSize: 24, color: "#FFF" }}>
@@ -20,35 +22,47 @@ function SecurityHeader() {
         are paramount, we are steadfast in our commitment to protecting your
         information with the utmost rigor.
       </Typography>
-    </Box>
+    </Container>
   );
 }
 
 export default function Security() {
   return (
-    <Box sx={{ background: "#0C0E0F", display: "flex", justifyContent: "center", overflowX: "hidden" }}>
-      <Box sx={{py: {md: 10}, display: "flex", alignItems: "center", gap: 10, maxWidth: 1505}}>
-          <Box
-            component="img"
-            src="/security3.png"
-            sx={{
-              width: { xs: 175, md: 327 },
-              height: { xs: 127, md: 239 },
-              objectFit: "cover",
-              mt: { md: 0, xs: 9 },
-            }}
-          />
-          <SecurityHeader />
-          <Box
-            component="img"
-            src="/security4.png"
-            sx={{
-              width: { xs: 298, md: 406 },
-              height: { xs: 175, md: 235, },
-              mt: { md: 0, xs: -13 },
-              mb: { md: 0, xs: 7.5 },
-            }}
-          />
+    <Box
+      sx={{
+        background: "#0C0E0F",
+        display: "flex",
+        justifyContent: "center",
+        overflowX: "hidden",
+      }}
+    >
+      <Box
+        sx={{
+          py: { xs: 7, md: 10 },
+          display: "flex",
+          flexDirection: { xs: "column", xl: "row-reverse" },
+          alignItems: "center",
+          gap: 10,
+          maxWidth: 1505,
+        }}
+      >
+        <Box
+          component="img"
+          src="/security4.png"
+          sx={{
+            width: { xs: 298, md: 376 },
+            height: { xs: 175, md: 221 },
+          }}
+        />
+        <SecurityHeader />
+        <Box
+          component="img"
+          src="/security3.png"
+          sx={{
+            width: { xs: 174, md: 327 },
+            height: { xs: 127, md: 239 },
+          }}
+        />
       </Box>
     </Box>
   );
