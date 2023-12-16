40079443c9fe029995ac41afb28c33f4d2a842ac
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-16 23:02:40 +0200

    Make Filepond browse file -> image preview flow.

--- src/app/activities/components/LogoUploadFilepond.js
+++ src/app/activities/components/LogoUploadFilepond.js
@@ -9,22 +9,24 @@ import "filepond/dist/filepond.min.css";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import Box from "@mui/material/Box";
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 export default function LogoUploadFilepond() {
   const [files, setFiles] = useState([]);
   return (
-    <div className="App">
+    <Box sx={{ width: 380}}>
       <FilePond
         files={files}
         onupdatefiles={setFiles}
-        allowMultiple={true}
         maxFiles={3}
         server="/api"
         name="files"
         labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+        credits={false}
+        instantUpload={false}
       />
-    </div>
+    </Box>
   );
 }
