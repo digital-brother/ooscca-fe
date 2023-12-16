8ebaaac573cdc2ea4f3d2c81724ec6326f74cbb1
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-16 22:34:03 +0200

    Filepond clean comments

--- src/app/activities/components/ImageUploadFilePond.js
+++ src/app/activities/components/ImageUploadFilePond.js
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-// Import React FilePond
 import { FilePond, registerPlugin } from 'react-filepond';
 
-// Import FilePond styles
 import 'filepond/dist/filepond.min.css';
 
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
-// Register the plugins
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
-// Our app
 function App() {
     const [files, setFiles] = useState([]);
     return (
@@ -34,6 +27,3 @@ function App() {
         </div>
     );
 }
-
-// Attach App to DOM
-ReactDOM.render(<App />, document.getElementById('root'));
