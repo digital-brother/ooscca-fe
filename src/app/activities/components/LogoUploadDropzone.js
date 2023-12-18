3e7f44f3d8861b21e5e55dc34e95e4b7c5651180
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-18 20:41:47 +0200

    Clean up dropzone example styles

--- src/app/activities/components/LogoUploadDropzone.js
+++ src/app/activities/components/LogoUploadDropzone.js
@@ -5,37 +5,6 @@ import { useDropzone } from "react-dropzone";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-const thumbsContainer = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  marginTop: 16,
-};
-
-const thumb = {
-  display: "inline-flex",
-  borderRadius: 2,
-  border: "1px solid #eaeaea",
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: "border-box",
-};
-
-const thumbInner = {
-  display: "flex",
-  minWidth: 0,
-  overflow: "hidden",
-};
-
-const img = {
-  display: "block",
-  width: "auto",
-  height: "100%",
-};
-
 function LogoInput({ setFiles }) {
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
