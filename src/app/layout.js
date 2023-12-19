c75617eebbd224cc9cf69fe3ec4618ddb8ad3ca1
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-19 20:17:36 +0200

    Add QueryClientProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,25 @@
-import * as React from 'react';
-import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+"use client"
+
+import * as React from "react";
+import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 import Header from "@/app/Header";
 import Footer from "@/app/Footer";
+import {QueryClient, QueryClientProvider} from "react-query";
 
-export const metadata = {
-  title: 'OOSCCA',
-  description: 'OOSCCA',
-};
+const queryClient = new QueryClient()
 
-
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <body>
-    <ThemeRegistry>
-      <Header/>
-      {children}
-      <Footer/>
-    </ThemeRegistry>
-    </body>
+      <body>
+        <ThemeRegistry>
+          <QueryClientProvider client={queryClient}>
+            <Header />
+            {children}
+            <Footer />
+          </QueryClientProvider>
+        </ThemeRegistry>
+      </body>
     </html>
   );
 }
