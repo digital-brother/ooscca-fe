5898aea1fcbb02e33832158b98cfa0494ad6915c
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-20 02:34:30 +0200

    Reformat

--- src/components/ThemeRegistry/theme.js
+++ src/components/ThemeRegistry/theme.js
@@ -1,65 +1,65 @@
-import {createTheme} from '@mui/material/styles';
-import {Libre_Franklin, Manrope, Montserrat} from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+import { Libre_Franklin, Manrope, Montserrat } from "next/font/google";
 
 const libreFranklin = Libre_Franklin({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap',
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const manrope = Manrope({
-    weight: ['400', '600', '700'],
-    subsets: ['latin'],
-    display: 'swap',
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const montserrat = Montserrat({
-    weight: ['400', '700'],
-    subsets: ['latin'],
-    display: 'swap',
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const baseTheme = createTheme();
 
 const theme = createTheme({
-    palette: {
-        mode: 'light',
+  palette: {
+    mode: "light",
+  },
+  typography: {
+    fontFamily: libreFranklin.style.fontFamily,
+    h1: {
+      fontSize: "3rem", // 48px
+      fontWeight: 700,
     },
-    typography: {
-        fontFamily: libreFranklin.style.fontFamily,
-        h1: {
-            fontSize: '3rem', // 48px
-            fontWeight: 700,
-        },
-        h2: {
-            fontSize: '2.5rem', // 40px
-            fontWeight: 700,
-        },
-        h3: {
-            fontSize: '2rem', // 32px
-            fontWeight: 700,
-        },
-        h5: {
-            fontSize: '1.5rem', //24
-            fontWeight: 700,
-        },
-        subheading: {
-            fontSize: '1rem', // 16px
-            fontWeight: 700,
-            color: baseTheme.palette.warning.main,
-        },
+    h2: {
+      fontSize: "2.5rem", // 40px
+      fontWeight: 700,
     },
-    components: {
-        MuiAlert: {
-            styleOverrides: {
-                root: ({ownerState}) => ({
-                    ...(ownerState.severity === 'info' && {
-                        backgroundColor: '#60a5fa',
-                    }),
-                }),
-            },
-        },
+    h3: {
+      fontSize: "2rem", // 32px
+      fontWeight: 700,
     },
+    h5: {
+      fontSize: "1.5rem", //24
+      fontWeight: 700,
+    },
+    subheading: {
+      fontSize: "1rem", // 16px
+      fontWeight: 700,
+      color: baseTheme.palette.warning.main,
+    },
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
+          }),
+        }),
+      },
+    },
+  },
 });
 
 export default theme;
