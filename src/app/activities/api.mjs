68649e43eb33566266e60a5593f413332334d8e1
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-21 16:47:00 +0200

    Refactor api.mjs

--- src/app/activities/api.mjs
+++ src/app/activities/api.mjs
@@ -11,8 +11,6 @@ const PROVIDERS_PATH = "providers/";
 const ACTIVITY_TYPES_PATH = "activity-types/";
 const ACTIVITIES_PATH = "activities/";
 
-// TODO: Remove once providers page is set
-
 const client = axios.create({
   baseURL: API_HOST,
   timeout: 1000,
@@ -22,32 +20,18 @@ const client = axios.create({
 });
 
 export function patchActivity(activityId, data) {
-  const url = `${ACTIVITIES_PATH}${activityId}/`
+  const url = `${ACTIVITIES_PATH}${activityId}/`;
   return client.patch(url, data);
 }
 
 // TODO: Unify logic with provider api functions
-export async function getActivityTypes() {
-  const response = await client.get(ACTIVITY_TYPES_PATH);
-  return response.data;
+export function getActivityTypes() {
+  return client.get(ACTIVITY_TYPES_PATH);
 }
 
-// TODO: remove once providers page is set
-// export async function getProviders() {
-//   const response = await client.get(PROVIDERS_PATH);
-//   return response.data;
-// }
-
-export async function getProvider(providerId) {
-  const response = await client.get(`${PROVIDERS_PATH}${providerId}/`);
-  return response.data;
+export function getProvider(providerId) {
+  return client.get(`${PROVIDERS_PATH}${providerId}/`);
 }
-
-// TODO: remove once providers page is set
-// export async function postProvider(url, {arg: data}) {
-//   const response = await client.post(PROVIDERS_PATH, data);
-//   return response.data;
-// }
 
 export async function patchProvider(providerId, data, file) {
   let formData = new FormData();
@@ -64,9 +48,3 @@ export async function patchProvider(providerId, data, file) {
   const response = await client.patch(`${PROVIDERS_PATH}${providerId}/`, formData);
   return response.data;
 }
-
-// TODO: remove once providers page is set
-// export async function deleteProvider(providerId) {
-//   const response = await client.delete(`${PROVIDERS_PATH}${providerId}/`);
-//   return response.data;
-// }
