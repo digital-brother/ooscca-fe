77e2f70ef7f5ef7e46b2a0610745fca072ab267a
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-25 23:15:26 +0200

    Make FormikTimeField working

--- src/app/activities/components/formikFields.js
+++ src/app/activities/components/formikFields.js
@@ -2,6 +2,8 @@ import { useField } from "formik";
 import TextField from "@mui/material/TextField";
 import React from "react";
 import { Checkbox, FormControlLabel } from "@mui/material";
+import { TimeField } from "@mui/x-date-pickers/TimeField";
+import dayjs from "dayjs";
 
 // TODO: Rationalize on how to handle errors
 // Handles cases, when value initial value is:
@@ -54,4 +56,31 @@ export function FormikCheckboxField({ label, ...props }) {
   const fieldValue = field.value ?? false;
 
   return <FormControlLabel control={<Checkbox {...field} value={fieldValue} {...props} />} label={label} />;
+}
+
+export function FormikTimeField(props) {
+  const [field, meta, helpers] = useField(props);
+  // const initialValue = dayjs("22:16:37", "HH:mm")
+  // const initialValue = null
+  // const [value, setValue] = useState(initialValue);
+
+  console.log(field);
+
+  const fieldValue = dayjs(field.value, "HH:mm");
+
+  function handleChange(value) {
+    const formikValue = value?.format("HH:mm")
+    helpers.setValue(formikValue);
+  }
+
+  // const fieldValue = dayjs(value);
+  // const [field, meta] = useField(props);
+
+  // const isString = typeof field.value === "string";
+  // const fieldValue = isString ? dayjs(field.value, "HH:mm") : field.value;
+
+  // return <TimeField {...field} value={fieldValue} {...props} />;
+
+  return <TimeField value={fieldValue} onChange={handleChange} />;
+  // return <TimeField value={value} onChange={(value) => setValue(value)} {...props} />;
 }
