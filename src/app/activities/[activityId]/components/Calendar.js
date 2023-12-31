c5d7725576cff7f062e3fa54c9d3deebd0a25672
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-31 03:34:32 +0200

    Make components

--- src/app/activities/[activityId]/components/Calendar.js
+++ src/app/activities/[activityId]/components/Calendar.js
@@ -1,8 +1,32 @@
+"use client";
+
 import React from "react";
 import dayjs from "dayjs";
 import { Box, Typography } from "@mui/material";
 
-export default function Calendar() {
+function CalendarCssGrid({ children, sx, props }) {
+  return (
+    <Box sx={{ display: "grid", gridTemplateColumns: "repeat(7, 1fr)", ...sx }} {...props}>
+      {children}
+    </Box>
+  );
+}
+
+function DayNames() {
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return (
+    <CalendarCssGrid sx={{ mt: 3 }}>
+      {daysOfWeek.map((day, index) => (
+        <Typography sx={{ fontWeight: 700 }} key={`day-${index}`}>
+          {day}
+        </Typography>
+      ))}
+    </CalendarCssGrid>
+  );
+}
+
+function Days() {
   const now = dayjs();
 
   const startMonthDayOfWeek = now.startOf("month").day();
@@ -11,31 +35,29 @@ export default function Calendar() {
   const monthDaysNumber = now.daysInMonth();
   const monthDaysNumbersArray = Array.from({ length: monthDaysNumber }, (_, index) => index + 1);
 
-  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return (
+    <CalendarCssGrid sx={{ mt: 1 }}>
+      {emptyDaysNumbersArray.map((_, index) => (
+        <Box key={`empty-${index}`}></Box>
+      ))}
+
+      {monthDaysNumbersArray.map((dayNumber) => (
+        <Typography sx={{ border: "1px solid black" }} key={dayNumber}>
+          {dayNumber}
+        </Typography>
+      ))}
+    </CalendarCssGrid>
+  );
+}
+
+export default function Calendar() {
+  const now = dayjs();
 
   return (
     <Box sx={{ maxWidth: 500, mx: "auto" }}>
       <Typography variant="h5">{now.format("MMMM YYYY")}</Typography>
-
-      <Box sx={{ display: "grid", gridTemplateColumns: "repeat(7, 1fr)", mt: 3 }}>
-        {daysOfWeek.map((day, index) => (
-          <Typography sx={{ fontWeight: 700 }} key={`day-${index}`}>
-            {day}
-          </Typography>
-        ))}
-      </Box>
-
-      <Box sx={{ display: "grid", gridTemplateColumns: "repeat(7, 1fr)", mt: 1 }}>
-        {emptyDaysNumbersArray.map((_, index) => (
-          <Box key={`empty-${index}`}></Box>
-        ))}
-
-        {monthDaysNumbersArray.map((day) => (
-          <Typography sx={{ border: "1px solid black" }} key={day}>
-            {day}
-          </Typography>
-        ))}
-      </Box>
+      <DayNames />
+      <Days />
     </Box>
   );
 }
