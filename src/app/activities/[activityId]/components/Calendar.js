6d82727a98126394845a3d10f2bbf4d2ccee3a04
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-12-31 02:46:59 +0200

    Display days of week

--- src/app/activities/[activityId]/components/Calendar.js
+++ src/app/activities/[activityId]/components/Calendar.js
@@ -11,10 +11,17 @@ export default function Calendar() {
   const monthDaysNumber = now.daysInMonth();
   const monthDaysNumbersArray = Array.from({ length: monthDaysNumber }, (_, index) => index + 1);
 
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   return (
     <div>
       <h2>{now.format("MMMM YYYY")}</h2>
       <Box sx={{ display: "grid", gridTemplateColumns: "repeat(7, 1fr)" }}>
+        {daysOfWeek.map((day, index) => (
+          <Box key={`day-${index}`} sx={{ fontWeight: "bold" }}>
+            {day}
+          </Box>
+        ))}
         {emptyDaysNumbersArray.map((_, index) => (
           <Box key={`empty-${index}`}></Box>
         ))}
