a3ae728e05830c3c170a44769d3db3db2c97bae3
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-01-19 15:17:53 +0200

    Refactor slashes handling

--- src/app/activities/[activityId]/api.mjs
+++ src/app/activities/[activityId]/api.mjs
@@ -5,10 +5,10 @@ const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
 const AUTH_TOKEN = process.env.NEXT_PUBLIC_AUTH_TOKEN;
 
 // TODO: Update, so that urls explicitly include slashes
-const PROVIDERS_PATH = "providers/";
-const ACTIVITY_TYPES_PATH = "activity-types/";
-const ACTIVITIES_PATH = "activities/";
-const DISCOUNTS_PATH = "discounts/";
+const PROVIDERS_PATH = "/providers";
+const ACTIVITY_TYPES_PATH = "/activity-types";
+const ACTIVITIES_PATH = "/activities";
+const DISCOUNTS_SUBPATH = "discounts";
 
 const client = axios.create({
   baseURL: API_HOST,
@@ -19,46 +19,41 @@ const client = axios.create({
 });
 
 export async function createDiscount(activityId, data) {
-    const url = `${ACTIVITIES_PATH}${activityId}/${DISCOUNTS_PATH}`;
-    const response = await client.post(url, data);
-    return response.data;
- }
-
+  const url = `${ACTIVITIES_PATH}/${activityId}/${DISCOUNTS_SUBPATH}/`;
+  const response = await client.post(url, data);
+  return response.data;
+}
 export async function getActivityDiscounts(activityId) {
-  const url = `${ACTIVITIES_PATH}${activityId}/${DISCOUNTS_PATH}`;
+  const url = `${ACTIVITIES_PATH}/${activityId}/${DISCOUNTS_SUBPATH}/`;
   const response = await client.get(url);
   return response.data;
 }
-
 export async function patchDiscount(activityId, discountId, data) {
-  const url = `${ACTIVITIES_PATH}${activityId}/${DISCOUNTS_PATH}${discountId}/`;
+  const url = `${ACTIVITIES_PATH}/${activityId}/${DISCOUNTS_SUBPATH}/${discountId}/`;
   const response = await client.patch(url, data);
   return response.data;
 }
 
 export async function getActivity(activityId) {
-  const url = `${ACTIVITIES_PATH}${activityId}/`;
+  const url = `${ACTIVITIES_PATH}/${activityId}/`;
   const response = await client.get(url);
   return response.data;
 }
-
 export async function patchActivity(activityId, data) {
-  const url = `${ACTIVITIES_PATH}${activityId}/`;
+  const url = `${ACTIVITIES_PATH}/${activityId}/`;
   const response = await client.patch(url, data);
   return response.data;
 }
-
 // TODO: Unify logic with provider api functions
 export async function getActivityTypes() {
-  const response = await client.get(ACTIVITY_TYPES_PATH);
+  const response = await client.get(`${ACTIVITY_TYPES_PATH}/`);
   return response.data;
 }
 
 export async function getProvider(providerId) {
-  const response = await client.get(`${PROVIDERS_PATH}${providerId}/`);
+  const response = await client.get(`${PROVIDERS_PATH}/${providerId}/`);
   return response.data;
 }
-
 export async function patchProvider(providerId, data, file) {
   let formData = new FormData();
 
@@ -71,6 +66,6 @@ export async function patchProvider(providerId, data, file) {
     }
   }
 
-  const response = await client.patch(`${PROVIDERS_PATH}${providerId}/`, formData);
+  const response = await client.patch(`${PROVIDERS_PATH}/${providerId}/`, formData);
   return response.data;
 }
