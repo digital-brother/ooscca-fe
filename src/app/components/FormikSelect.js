9d39b46fa35b1c0dd28d069a068ba6fc233e2b6c
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-01-19 22:18:31 +0200

    init new select

--- src/app/components/FormikSelect.js
+++ src/app/components/FormikSelect.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { FormControl, FormHelperText, MenuItem, Select as MUISelect } from "@mui/material";
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select as MUISelect } from "@mui/material";
 import { ErrorMessage, useField } from "formik";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { v4 as uuid } from "uuid";
 
-export function FormikSelect({ label, items, sx, name, containerSx }) {
-  const [field, meta] = useField({ name });
+export function FormikSelect({
+  label,
+  items,
+  sx,
+  name,
+  containerSx,
+  itemValueField = "id",
+  itemLabelField = "name",
+  ...props
+}) {
+  const [field, meta] = useField({ name, type: "select" });
 
   const isError = meta.touched && Boolean(meta.error);
+  const labelId = uuid();
 
   return (
-    <FormControl sx={containerSx}>
+    <FormControl sx={containerSx} fullwidth>
+      <InputLabel id={labelId}>{label}</InputLabel>
       <MUISelect
         {...field}
-        displayEmpty
+        autoWidth
+        // displayEmpty
         variant="filled"
+        labelId={labelId}
+        label={label}
         disableUnderline
-        inputProps={{ sx: { py: 1 } }}
+        // inputProps={{ sx: { py: 1 } }}
         IconComponent={KeyboardArrowDownIcon}
         sx={{
           borderRadius: 1,
@@ -25,13 +40,14 @@ export function FormikSelect({ label, items, sx, name, containerSx }) {
           "& .MuiSelect-icon": {
             color: field.value ? "grey.800" : "grey.600",
           },
+          // height: 56,
           ...sx,
         }}
       >
         {label && <MenuItem value="">{label}</MenuItem>}
         {items.map((item, index) => (
-          <MenuItem key={index} value={item.id}>
-            {item.name}
+          <MenuItem key={index} value={item[itemValueField]}>
+            {item[itemLabelField]}
           </MenuItem>
         ))}
       </MUISelect>
@@ -40,6 +56,33 @@ export function FormikSelect({ label, items, sx, name, containerSx }) {
           <ErrorMessage name={name} />
         </FormHelperText>
       )}
+    </FormControl>
+  );
+}
+
+export function FormikSelectNew({ items=[], valueField, titleField }) {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+      <InputLabel id="demo-simple-select-filled-label">Age</InputLabel>
+      <MUISelect
+        labelId="demo-simple-select-filled-label"
+        id="demo-simple-select-filled"
+        value={age}
+        onChange={handleChange}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </MUISelect>
     </FormControl>
   );
 }
