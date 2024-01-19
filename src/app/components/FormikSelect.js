f39134bf3329399ded5c6ea802aa8c4dcc351844
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-01-19 22:27:47 +0200

    Dynamic items for FormikSelectNew

--- src/app/components/FormikSelect.js
+++ src/app/components/FormikSelect.js
@@ -75,9 +75,11 @@ export function FormikSelectNew({ items=[], valueField="id", titleField="name" }
         <MenuItem value="">
           <em>None</em>
         </MenuItem>
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {items.map((item) => (
+          <MenuItem key={item[valueField]} value={item[valueField]}>
+            {item[titleField]}
+          </MenuItem>
+        ))}
       </MUISelect>
       <FormHelperText>With label + helper text</FormHelperText>
     </FormControl>
