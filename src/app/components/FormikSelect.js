a73315ff2bdd84eecdb021ce9f5daee060185b42
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-01-22 20:38:12 +0200

    remove legacy formikSelect

--- src/app/components/FormikSelect.js
+++ src/app/components/FormikSelect.js
@@ -1,66 +1,9 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { FormControl, FormHelperText, InputLabel, MenuItem, Select as MUISelect } from "@mui/material";
-import { ErrorMessage, useField } from "formik";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { v4 as uuid } from "uuid";
+import { useField } from "formik";
 
-export function FormikSelect({
-  label,
-  items,
-  sx,
-  name,
-  containerSx,
-  itemValueField = "id",
-  itemLabelField = "name",
-  ...props
-}) {
-  const [field, meta] = useField({ name, type: "select" });
 
-  const isError = meta.touched && Boolean(meta.error);
-  const labelId = `${name}-select-label`;
-
-  return (
-    <FormControl sx={containerSx} >
-      <InputLabel id={labelId}>{label}</InputLabel>
-      <MUISelect
-        {...field}
-        autoWidth
-        // displayEmpty
-        variant="filled"
-        labelId={labelId}
-        label={label}
-        disableUnderline
-        // inputProps={{ sx: { py: 1 } }}
-        IconComponent={KeyboardArrowDownIcon}
-        sx={{
-          borderRadius: 1,
-          color: field.value ? "grey.800" : "grey.600",
-          fontWeight: 700,
-          textAlign: "center",
-          "& .MuiSelect-icon": {
-            color: field.value ? "grey.800" : "grey.600",
-          },
-          // height: 56,
-          ...sx,
-        }}
-      >
-        {label && <MenuItem value="">{label}</MenuItem>}
-        {items.map((item, index) => (
-          <MenuItem key={index} value={item[itemValueField]}>
-            {item[itemLabelField]}
-          </MenuItem>
-        ))}
-      </MUISelect>
-      {isError && (
-        <FormHelperText>
-          <ErrorMessage name={name} />
-        </FormHelperText>
-      )}
-    </FormControl>
-  );
-}
-
-export function FormikSelectNew({ name, items, label, sx, variant, valueField = "id", titleField = "name" }) {
+export function FormikSelect({ name, items, label, sx, variant, valueField = "id", titleField = "name" }) {
   const [field, meta] = useField(name);
 
   return (
