c21e081a79d2fafd14b78c2c8e30983678ea3e36
Slava Loievskyi <loievskyi.slava@gmail.com>
2024-01-29 17:04:47 +0200

    Changed structure

--- src/app/(homepage)/SecondaryImages.js
+++ src/app/(homepage)/SecondaryImages.js
@@ -17,21 +17,79 @@ import {
   postImage,
 } from "@/app/activities/apiImage.mjs";
 
-export default function SecondaryImages() {
+export function SecondaryImageMessages({ file }) {
+  let messageColor = ""
+  const messageTexts = []
 
-  const [files, setFiles] = useState([]);
-  const [isFilesLoaded, setIsFilesLoaded] = useState(false);
-  const numberOfElements = 3;
-  const imageInputs = [];
-  const activityId = useParams().activityId;
+  if (file) {
+    file.frontendErrors = []
+    if (file?.size > 5*1024*1024) {
+      file?.frontendErrors.push("image: Max file size is 5.0 MB")
+    }
+  }
 
-  function removeFile(sentImage) {
-    setFiles(files => files.filter(item => item.id !== sentImage?.id));
+  if (file?.error || file?.frontendErrors.length > 0) {
+    messageColor = "red.500"
+    if (file?.error?.response?.data) {  // 2 types errors: from backend & if network failed
+      for (const [key, value] of Object.entries(file?.error?.response?.data)) {
+        messageTexts.push(key + ": " + value)
+      }
+    } else if (file?.error?.message) {
+      messageTexts.push(file?.error?.message)
+    } else if (file?.frontendErrors?.length > 0) {
+      file?.frontendErrors?.map(frontendError => messageTexts.push(frontendError))
+    } else {
+      messageTexts.push("Unknown error")
+    }
+
+  } else {
+    messageColor = "green.500"
+    if (Object.prototype.isPrototypeOf.call(file, "error")) {
+      messageTexts.push("Image approved")
+    }
+  }
+
+  return (<Box>
+    {messageTexts.map((text, index) => (
+      <Box key={index}>
+        <Typography sx={{
+          color: messageColor,
+          fontSize: 16,
+          fontFamily: "Manrope",
+          fontStyle: "normal",
+          fontWeight: "400",
+          lineHeight: 1,
+          letterSpacing: 0.09,
+          mt: 0.8,
+        }}>
+          {text}
+        </Typography>
+      </Box>
+    ))}
+  </Box>)
+}
+
+export function SecondaryImageWidget({initialFiles, order, enabled, ...props}) {
+  // const activityId = useParams().activityId;
+  const activityId = 1;
+
+  const [widgetFiles, setWidgetFiles] = useState(initialFiles)  // array with 0 or 1 element
+  useEffect(() => {
+    setWidgetFiles(widgetFiles => initialFiles)
+  }, [initialFiles])
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => initialFiles.forEach((file) => URL.revokeObjectURL(file.url));
+  }, [initialFiles]);
+
+  function removeFile() {
+    setWidgetFiles(widgetFiles => []);
   }
 
   const deleteMutation = useMutation((data) => deleteImage(data?.id), {
     onSuccess: (data, sentImage, context) => {
-      removeFile(sentImage);
+      removeFile();
     },
     onError: (error, sentImage, context) => {
       sentImage.error = error
@@ -55,6 +113,59 @@ export default function SecondaryImages() {
     },
   });
 
+  async function handleDelete() {
+      widgetFiles.forEach((file) => {
+      if (file?.id) {
+        deleteMutation.mutateAsync(file)
+      } else {
+        removeFile();
+      }
+    })
+  }
+
+  async function handleAppend(acceptedFiles) {
+    setWidgetFiles(widgetFiles => [...widgetFiles, ...acceptedFiles.map((file, index) => {
+        const newObject = Object.assign(file, {
+          preview: URL.createObjectURL(file),
+          order,
+        })
+        postMutation.mutate(newObject)
+        return newObject
+      }),
+    ]);
+  }
+
+  return (
+    <Grid item md={4} sx={{ borderRadius: 8, width: "100%" }}>
+      <DropZoneImageUpload
+        files={widgetFiles}
+        order={order}
+        handleDelete={handleDelete}
+        handleAppend={handleAppend}
+        enabled={enabled}
+        sx={{
+          backgroundColor: "grey.200",
+          height: { xs: 232, md: 396 },
+          width: "100%",
+          borderRadius: 4,
+          border: "1px #ADB5BD solid",
+          enabled: {enabled},
+        }}
+      />
+      <SecondaryImageMessages file={widgetFiles[0]} />
+    </Grid>
+  )
+}
+
+export default function SecondaryImages() {
+
+  const [initialFiles, setinitialFiles] = useState([]);
+  const [isInitialFilesLoaded, setIsInitialFilesLoaded] = useState(false);
+  const numberOfElements = 3;
+  const imageInputs = [];
+  // const activityId = useParams().activityId;
+  const activityId = 1;
+
   const {
     data: images,
     isLoading,
@@ -62,114 +173,20 @@ export default function SecondaryImages() {
   } = useQuery({
     queryKey: "secondaryImages",
     queryFn: () => getSecondaryImages(),
-    enabled: !isFilesLoaded,  // disable repeated requests
-    onSuccess: () => {
-      setIsFilesLoaded(true);
+    enabled: !isInitialFilesLoaded,  // disable repeated requests
+    onSuccess: (data) => {
+      setinitialFiles(initialFiles => data);
+      setIsInitialFilesLoaded(true);
     }
   });
 
-  useEffect(() => {
-    if (!isLoading && !isError && images) {
-      setFiles(files => images);
-    }
-  }, [images, isLoading, isError]);
-
-  useEffect(() => {
-    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
-    return () => files.forEach((file) => URL.revokeObjectURL(file.url));
-  }, [files]);
-
-  for (let i = 1; i <= numberOfElements; i++) {
-    const _files = files.filter(f => f?.order === i)
-
-    async function handleDelete() {
-        _files.forEach((file) => {
-        if (file?.id) {
-          deleteMutation.mutateAsync(file)
-        } else {
-          setFiles(files => files.filter(item => item !== _files[0]));
-        }
-      })
-    }
-
-    async function handleAppend(acceptedFiles) {
-      setFiles(files => [...files, ...acceptedFiles.map((file, index) => {
-          const newObject = Object.assign(file, {
-            preview: URL.createObjectURL(file),
-            order: i,
-          })
-          postMutation.mutate(newObject)
-          return newObject
-        }),
-      ]);
-    }
-
-    let messageColor = ""
-    const messageTexts = []
-
-    if (_files[0]) {
-      _files[0].frontendErrors = []
-      if (_files[0]?.size > 5*1024*1024) {
-        _files[0]?.frontendErrors.push("image: Max file size is 5.0 MB")
-      }
-    }
-
-    if (_files[0]?.error || _files[0]?.frontendErrors.length > 0) {
-      messageColor = "red.500"
-      if (_files[0]?.error?.response?.data) {  // 2 types errors: from backend & if network failed
-        for (const [key, value] of Object.entries(_files[0]?.error?.response?.data)) {
-          messageTexts.push(key + ": " + value)
-        }
-      } else if (_files[0]?.error?.message) {
-        messageTexts.push(_files[0]?.error?.message)
-      } else if (_files[0]?.frontendErrors?.length > 0) {
-        _files[0]?.frontendErrors?.map(frontendError => messageTexts.push(frontendError))
-      } else {
-        messageTexts.push("Unknown error")
-      }
-
-    } else {
-      messageColor = "green.500"
-      if (Object.prototype.isPrototypeOf.call(_files[0], "error")) {
-        messageTexts.push("Image approved")
-      }
-    }
-
+  for (let order = 1; order <= numberOfElements; order++) {
     imageInputs.push(
-      <Grid key={i} item md={4} sx={{ borderRadius: 8, width: "100%" }}>
-        <DropZoneImageUpload
-          files={_files}
-          order={i}
-          handleDelete={handleDelete}
-          handleAppend={handleAppend}
-          key={i}
-          sx={{
-            backgroundColor: "grey.200",
-            height: { xs: 232, md: 396 },
-            width: "100%",
-            borderRadius: 4,
-            border: "1px #ADB5BD solid",
-          }}
-        />
-        <Box>
-          {messageTexts.map((text, index) => (
-            <Box key={index}>
-              <Typography sx={{
-                color: messageColor,
-                fontSize: 16,
-                fontFamily: "Manrope",
-                fontStyle: "normal",
-                fontWeight: "400",
-                lineHeight: 1,
-                letterSpacing: 0.09,
-                mt: 0.8,
-              }}>
-                {text}
-              </Typography>
-            </Box>
-          ))}
-        </Box>
-      </Grid>
+      <SecondaryImageWidget
+        initialFiles={initialFiles.filter((file) => file.order === order )}
+        order={order}
+        isInitialFilesLoaded={isInitialFilesLoaded}
+      />
     );
   }
 
