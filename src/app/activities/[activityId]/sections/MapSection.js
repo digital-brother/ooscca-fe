17bf79fe04414f59af707eacaaadabf04ef34073
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-02 13:00:23 +0200

    Refactor error handling

--- src/app/activities/[activityId]/sections/MapSection.js
+++ src/app/activities/[activityId]/sections/MapSection.js
@@ -10,15 +10,15 @@ import {Button, Container} from "@mui/material";
 import {Errors} from "@/app/activities/[activityId]/components/formikFields";
 
 export function MapSection() {
-  const defaultCoordinates = { lat: 51.5074, lng: -0.1278 };
   const activityId = useParams().activityId;
+  
+  const londonCoordinates = { lat: 51.5074, lng: -0.1278 };
+  const [coordinates, setCoordinates] = useState(londonCoordinates);
+  const [address, setAddress] = useState('');
+  const [errors, setErrors] = useState([]);
+  
   const queryClient = useQueryClient();
   const { data: activity } = useQuery(["activity", activityId], () => getActivity(activityId));
-  const [errors, setErrors] = useState([]);
-
-  const [coordinates, setCoordinates] = useState(defaultCoordinates);
-  const [address, setAddress] = useState('');
-
   const mutation = useMutation((data) => patchActivity(activityId, data));
 
   useEffect(() => {
@@ -35,25 +35,17 @@ export function MapSection() {
     const data = { latitude: coordinates.lat, longitude: coordinates.lng, address };
     mutation.mutate(data, {
       onSuccess: () => {
-        console.log("Success")
-        queryClient.invalidateQueries(['activity', activityId]);
+        // TODO: https://tanstack.com/query/v3/docs/framework/react/guides/updates-from-mutation-responses
+        queryClient.invalidateQueries(["activity", activityId]);
         setErrors([]);
       },
       onError: (error) => {
-        let errorMessages = [];
         const drfErrors = error.response?.data;
         const nonFieldErrors = error?.response?.data?.non_field_errors;
 
-        if (drfErrors && typeof drfErrors === 'object') {
-          errorMessages = Object.entries(drfErrors).flatMap(([field, errors]) => {
-            return errors.map((error) => `${field}: ${error}`);
-          });
-        } else if (nonFieldErrors && nonFieldErrors.length > 0) {
-          errorMessages.push(...nonFieldErrors);
-        } else {
-          errorMessages.push(error.message || 'An unexpected error occurred');
-        }
-        setErrors(errorMessages);
+        if (drfErrors) setErrors(Object.values(errors));
+        else if (nonFieldErrors && nonFieldErrors.length > 0) setErrors(...nonFieldErrors);
+        else setErrors(error.message);
       },
     });
   }
