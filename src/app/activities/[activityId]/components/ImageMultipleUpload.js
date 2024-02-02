716d412404c48752ee56d611444e9136e5b8992a
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-02 22:20:51 +0200

    Render loaded to widget files

--- src/app/activities/[activityId]/components/ImageMultipleUpload.js
+++ src/app/activities/[activityId]/components/ImageMultipleUpload.js
@@ -3,6 +3,10 @@
 import { Box, Container } from "@mui/system";
 import { ImageInput } from "./ImageUpload";
 import { useState } from "react";
+import { IconButton, Typography } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+import prettyBytes from "pretty-bytes";
 
 export default function ImageMultipleUpload() {
   const [files, setFiles] = useState([]);
@@ -10,6 +14,8 @@ export default function ImageMultipleUpload() {
   function handleAdd(files) {
     setFiles((previousFiles) => [...previousFiles, ...files]);
   }
+
+  // TODO: function handleDelete() {}
 
   console.log(files);
 
@@ -19,6 +25,18 @@ export default function ImageMultipleUpload() {
         sx={{ height: 110, overflow: "hidden", border: "1px #ADB5BD solid", borderRadius: 1.5, bgcolor: "grey.200" }}
       >
         <ImageInput multiple={true} handleAdd={handleAdd} />
+      </Box>
+      <Box>
+        {files.map((file, index) => (
+          <>
+            <Typography key={index}>{file.name}</Typography>
+            <Typography key={index}>{index}</Typography>
+            <Typography key={index}>{prettyBytes(file.size)}</Typography>
+            <IconButton>
+              <DeleteForeverIcon />
+            </IconButton>
+          </>
+        ))}
       </Box>
     </Container>
   );
