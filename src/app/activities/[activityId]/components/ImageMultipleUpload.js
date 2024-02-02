e559c35dccfa158a65dd9b7899a850beb64ec687
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-03 01:08:27 +0200

    Extract ImageMultiplePreview

--- src/app/activities/[activityId]/components/ImageMultipleUpload.js
+++ src/app/activities/[activityId]/components/ImageMultipleUpload.js
@@ -8,13 +8,71 @@ import { ImageInput } from "./ImageUpload";
 
 import prettyBytes from "pretty-bytes";
 import Image from "next/image";
+import { useDrag, useDrop } from "react-dnd";
+
+function ImageMultiplePreview({ filesData, handleDelete }) {
+  const [, drag] = useDrag(() => ({
+    type: "imagePreview",
+    // collect: (monitor) => ({
+    //   isDragging: !!monitor.isDragging(),
+    // }),
+  }));
+
+  // const [, drop] = useDrop(
+  //   () => ({
+  //     accept: "imagePreview",
+  //     drop: () => setImageOrder(order),
+  //   }),
+  //   [order]
+  // );
+
+  return (
+    <TableContainer sx={{ mt: 5 }}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow ref={drag}>
+            <TableCell></TableCell>
+            <TableCell align="right">Thumbnail</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Position</TableCell>
+            <TableCell align="right">Size</TableCell>
+            <TableCell align="right">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filesData.map((fileData, index) => (
+            <TableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+              <TableCell component="th" scope="row">
+                {"|||"}
+              </TableCell>
+              <TableCell align="right">
+                <Image src={fileData.url} alt="thumbnail" width="50" height="50" />
+              </TableCell>
+              <TableCell align="right">{fileData.file.name}</TableCell>
+              <TableCell align="right">{fileData.order}</TableCell>
+              <TableCell align="right">{prettyBytes(fileData.file.size)}</TableCell>
+              <TableCell align="right">
+                <IconButton onClick={() => handleDelete(index)}>
+                  <DeleteForeverIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 export default function ImageMultipleUpload() {
   const [filesData, setFilesData] = useState([]);
 
   function handleAdd(files) {
-    const filesData = files.map((file) => ({ file, url: URL.createObjectURL(file) }));
-    setFilesData((previousFilesData) => [...previousFilesData, ...filesData]);
+    const newFilesData = files.map((file, index) => {
+      const filesCount = filesData.length;
+      return { file, url: URL.createObjectURL(file), order: index + filesCount + 1 };
+    });
+    setFilesData((previousFilesData) => [...previousFilesData, ...newFilesData].sort((a, b) => a.order - b.order));
   }
 
   function handleDelete(deleteIndex) {
@@ -23,51 +81,14 @@ export default function ImageMultipleUpload() {
 
   useEffect(() => {
     return () => filesData.forEach((fileData) => URL.revokeObjectURL(fileData.url));
-  }, [filesData]);
+  }, []);
 
   return (
     <Container sx={{ my: 10 }}>
-      <Box
-        sx={{ height: 110, overflow: "hidden", border: "1px #ADB5BD solid", borderRadius: 1.5, bgcolor: "grey.200" }}
-      >
+      <Box sx={{ height: 110, border: "1px #ADB5BD solid", borderRadius: 1.5, bgcolor: "grey.200" }}>
         <ImageInput multiple={true} handleAdd={handleAdd} />
       </Box>
-      {filesData?.length > 0 && (
-        <TableContainer sx={{ mt: 5 }}>
-          <Table sx={{ minWidth: 650 }}>
-            <TableHead>
-              <TableRow>
-                <TableCell></TableCell>
-                <TableCell align="right">Thumbnail</TableCell>
-                <TableCell align="right">Name</TableCell>
-                <TableCell align="right">Position</TableCell>
-                <TableCell align="right">Size</TableCell>
-                <TableCell align="right">Delete</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filesData.map((fileData, index) => (
-                <TableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                  <TableCell component="th" scope="row">
-                    {"|||"}
-                  </TableCell>
-                  <TableCell align="right">
-                    <Image src={fileData.url} alt="thumbnail" width="50" height="50" />
-                  </TableCell>
-                  <TableCell align="right">{fileData.file.name}</TableCell>
-                  <TableCell align="right">{index + 1}</TableCell>
-                  <TableCell align="right">{prettyBytes(fileData.file.size)}</TableCell>
-                  <TableCell align="right">
-                    <IconButton onClick={() => handleDelete(index)}>
-                      <DeleteForeverIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
+      {filesData?.length > 0 && <ImageMultiplePreview {...{ filesData, handleDelete }} />}
     </Container>
   );
 }
