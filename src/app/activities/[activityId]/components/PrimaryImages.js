e9b70e093075788f431eb511d9203f9fa09916ae
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-02 21:27:04 +0200

    Reformat

--- src/app/activities/[activityId]/components/PrimaryImages.js
+++ src/app/activities/[activityId]/components/PrimaryImages.js
@@ -1,12 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import {Box, Button, Container} from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useDrag, useDrop, DndProvider } from "react-dnd";
 import { useParams } from "next/navigation";
 
-import {ImageInput} from "@/app/activities/[activityId]/components/ImageUploadDropzone";
+import { ImageInput } from "@/app/activities/[activityId]/components/ImageUploadDropzone";
 import { useMutation, useQuery } from "react-query";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -20,25 +20,19 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { TrashCanIcon } from "@/assets/TrashCanIcon";
 
-import {
-  getPrimaryImages,
-  patchImage,
-  deleteImage,
-  postImage,
-} from "@/app/activities/apiImage.mjs";
-
+import { getPrimaryImages, patchImage, deleteImage, postImage } from "@/app/activities/apiImage.mjs";
 
 function formatBytes(bytes, decimals) {
-  if(bytes === 0) return "0 Bytes";
-    const k = 1024
-    const dm = decimals || 2
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  if (bytes === 0) return "0 Bytes";
+  const k = 1024;
+  const dm = decimals || 2;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
-function ImageDataRow({ files, setFiles, file, _key, ...props  }) {
-  let opacity = 1
+function ImageDataRow({ files, setFiles, file, _key, ...props }) {
+  let opacity = 1;
   const [, drag] = useDrag({
     type: "ROW",
     item: file,
@@ -54,38 +48,40 @@ function ImageDataRow({ files, setFiles, file, _key, ...props  }) {
 
         const newFiles = [...files];
         newFiles.splice(hoverIndex, 0, newFiles.splice(draggedIndex, 1)[0]);
-        setFiles(files => newFiles);
+        setFiles((files) => newFiles);
       }
     },
   });
 
-  file.order = _key
+  file.order = _key;
 
   function handleDelete(event) {
-    file.markedForDeleting = true
-    delete file?.error
-    file = {...file}
+    file.markedForDeleting = true;
+    delete file?.error;
+    file = { ...file };
 
-    setFiles(files => files.filter(f => f !== null));
+    setFiles((files) => files.filter((f) => f !== null));
   }
 
-  let messageColor = ""
+  let messageColor = "";
 
   if (file?.markedForDeleting) {
-    opacity = 0.3
+    opacity = 0.3;
   }
 
-  return <TableRow
+  return (
+    <TableRow
       sx={{
         "&:last-child td, &:last-child th": { border: 0 },
         opacity,
-        ...props?.sx
+        ...props?.sx,
       }}
-      key={ _key }
+      key={_key}
       ref={(node) => drag(drop(node))}
     >
       <TableCell component="th" scope="row" align="center">
-        <Box component="img"
+        <Box
+          component="img"
           src={file.preview || file.image}
           sx={{
             height: 33,
@@ -96,7 +92,8 @@ function ImageDataRow({ files, setFiles, file, _key, ...props  }) {
       </TableCell>
       <TableCell component="th" scope="row">
         {file.name}
-        <Box sx={{
+        <Box
+          sx={{
             color: messageColor,
             fontSize: 16,
             fontFamily: "Manrope",
@@ -104,11 +101,13 @@ function ImageDataRow({ files, setFiles, file, _key, ...props  }) {
             mt: 0.8,
           }}
         >
-          {messageTexts.map((text, index) => (<Box key={index}>{text}</Box>))}
+          {messageTexts.map((text, index) => (
+            <Box key={index}>{text}</Box>
+          ))}
         </Box>
       </TableCell>
       <TableCell component="th" scope="row" align="center">
-        { file.order }
+        {file.order}
       </TableCell>
       <TableCell component="th" scope="row" align="center">
         {formatBytes(file.size, 1)}
@@ -119,9 +118,10 @@ function ImageDataRow({ files, setFiles, file, _key, ...props  }) {
         </Button>
       </TableCell>
     </TableRow>
+  );
 }
 
-function FilesTable({files, setFiles}) {
+function FilesTable({ files, setFiles }) {
   const [isDndReady, setIsDndReady] = useState(false);
   useEffect(() => {
     setIsDndReady(true);
@@ -129,21 +129,26 @@ function FilesTable({files, setFiles}) {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <TableContainer component={Paper} sx={{ border: "none", boxShadow: "none"}}>
-        <Table sx={{ border: "none", boxShadow: "none"}} aria-label="simple table">
+      <TableContainer component={Paper} sx={{ border: "none", boxShadow: "none" }}>
+        <Table sx={{ border: "none", boxShadow: "none" }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="center" width="15%">Thumbnail</TableCell>
-              <TableCell align="left" width="50%">Name and status </TableCell>
+              <TableCell align="center" width="15%">
+                Thumbnail
+              </TableCell>
+              <TableCell align="left" width="50%">
+                Name and status{" "}
+              </TableCell>
               <TableCell align="center">Position</TableCell>
               <TableCell align="center">Size</TableCell>
               <TableCell align="center">Delete</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {Array.isArray(files) && (files.map((file, key) => (
-              <ImageDataRow files={files} setFiles={setFiles} file={file} key={key+1} _key={key+1}/>
-            )))}
+            {Array.isArray(files) &&
+              files.map((file, key) => (
+                <ImageDataRow files={files} setFiles={setFiles} file={file} key={key + 1} _key={key + 1} />
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -152,13 +157,13 @@ function FilesTable({files, setFiles}) {
 }
 
 export default function PrimaryImages() {
-  const [filesData, setFilesData] = useState([])
+  const [filesData, setFilesData] = useState([]);
   const [isFilesLoaded, setIsFilesLoaded] = useState(false);
   // const activityId = useParams().activityId
-  const activityId = 1
+  const activityId = 1;
 
   function removeFile(imagData) {
-    setFilesData(filesData => filesData.filter(fileData => fileData.image !== imagData));
+    setFilesData((filesData) => filesData.filter((fileData) => fileData.image !== imagData));
   }
 
   function handleErrors(imageData, mutationErrors) {
@@ -171,32 +176,42 @@ export default function PrimaryImages() {
 
   const deleteMutation = useMutation((imageData) => deleteImage(data?.id), {
     onSuccess: (data, sentImage) => removeFile(sentImage),
-    onError: (errors, sentImage) => handleErrors(sentImage, errors)
-  });
-
-  const patchMutation = useMutation((imageData) => patchImage({
-      "id": imageData.id,
-      "name": imageData.name,
-      "order": imageData.order,
-    }), {
-    onSuccess: (data, sentImage, context) => { sentImage.errors = [] },
-    onError: (errors, sentImage) => handleErrors(sentImage, errors)
-  });
-
-  const postMutation = useMutation((imageData) => postImage({
-      "name": imageData.name,
-      "order": imageData.order,
-      "size": imageData.size,
-      "activity": activityId,
-      "type": "primary",
-      "image": imageData,
-    }), {
-    onSuccess: (data, sentImage) => {
-      sentImage.errors  = null
-      sentImage.id = data.id
-    },
     onError: (errors, sentImage) => handleErrors(sentImage, errors),
   });
+
+  const patchMutation = useMutation(
+    (imageData) =>
+      patchImage({
+        id: imageData.id,
+        name: imageData.name,
+        order: imageData.order,
+      }),
+    {
+      onSuccess: (data, sentImage, context) => {
+        sentImage.errors = [];
+      },
+      onError: (errors, sentImage) => handleErrors(sentImage, errors),
+    }
+  );
+
+  const postMutation = useMutation(
+    (imageData) =>
+      postImage({
+        name: imageData.name,
+        order: imageData.order,
+        size: imageData.size,
+        activity: activityId,
+        type: "primary",
+        image: imageData,
+      }),
+    {
+      onSuccess: (data, sentImage) => {
+        sentImage.errors = null;
+        sentImage.id = data.id;
+      },
+      onError: (errors, sentImage) => handleErrors(sentImage, errors),
+    }
+  );
 
   const {
     data: images,
@@ -205,15 +220,15 @@ export default function PrimaryImages() {
   } = useQuery({
     queryKey: "primaryImages",
     queryFn: () => getPrimaryImages(),
-    enabled: !isFilesLoaded,  // disable repeated requests
+    enabled: !isFilesLoaded, // disable repeated requests
     onSuccess: (data) => {
       setIsFilesLoaded(true);
-    }
+    },
   });
 
   useEffect(() => {
     if (!isLoading && !isError && images) {
-      setFiles(files => images);
+      setFiles((files) => images);
     }
   }, [images, isLoading, isError]);
 
@@ -224,13 +239,12 @@ export default function PrimaryImages() {
 
   async function deleteMarkedForDeletingImages() {
     // remove not uploaded files
-    setFilesData(filesData => filesData.filter(
-      f => f.id || (!f.id  && !(f?.markedForDeleting))));
+    setFilesData((filesData) => filesData.filter((f) => f.id || (!f.id && !f?.markedForDeleting)));
 
     // remove from backend
     const deletePromises = filesData
-      .filter(filesData => filesData.id && filesData?.markedForDeleting === true)
-      .map(file => deleteMutation.mutateAsync(file));
+      .filter((filesData) => filesData.id && filesData?.markedForDeleting === true)
+      .map((file) => deleteMutation.mutateAsync(file));
 
     // Wait for all delete mutations to complete
     await Promise.all(deletePromises);
@@ -239,9 +253,9 @@ export default function PrimaryImages() {
   async function updateUploadedImages() {
     // createdInBackendFilesAndNotMarkedForDeleting
     const patchPromises = filesData
-      .filter(file => file.id)
-      .filter(file => file?.markedForDeleting !== true)
-      .map(file => patchMutation.mutateAsync(file));
+      .filter((file) => file.id)
+      .filter((file) => file?.markedForDeleting !== true)
+      .map((file) => patchMutation.mutateAsync(file));
 
     // Wait for all patch mutations to complete
     await Promise.all(patchPromises);
@@ -250,10 +264,10 @@ export default function PrimaryImages() {
   async function uploadNewImages() {
     // notCreatedInBackendFilesWithoutErrorsAndNotMarkedForDeleting
     const postPromises = filesData
-      .filter(file => !file.id)
-      .filter(file => !(file?.frontendErrors.length > 0))
-      .filter(file => file?.markedForDeleting !== true)
-      .map(file => postMutation.mutateAsync(file));
+      .filter((file) => !file.id)
+      .filter((file) => !(file?.frontendErrors.length > 0))
+      .filter((file) => file?.markedForDeleting !== true)
+      .map((file) => postMutation.mutateAsync(file));
 
     // Wait for all post mutations to complete
     await Promise.all(postPromises);
@@ -263,32 +277,31 @@ export default function PrimaryImages() {
     if (Array.isArray(filesData)) {
       try {
         filesData.map((fileData, index) => {
-          fileData.errors = []
+          fileData.errors = [];
         });
 
-        await deleteMarkedForDeletingImages()
-        await updateUploadedImages()
-        await uploadNewImages()
-
+        await deleteMarkedForDeletingImages();
+        await updateUploadedImages();
+        await uploadNewImages();
 
         function getFilesExistedInBackend(files) {
-          return filesData.filter(file => file.id)
+          return filesData.filter((file) => file.id);
         }
 
         function getFilesNotExistedInBackend(files) {
-          return filesData.filter(file => !file.id)
+          return filesData.filter((file) => !file.id);
         }
 
         function getFilesWithoutFrontendErrors(files) {
-          return filesData.filter(file => !(file?.frontendErrors.length > 0))
+          return filesData.filter((file) => !(file?.frontendErrors.length > 0));
         }
 
         function getFilesMarkedForDeleting(files) {
-          return filesData.filter(file => file?.markedForDeleting === true)
+          return filesData.filter((file) => file?.markedForDeleting === true);
         }
 
         function getFilesNotMarkedForDeleting(files) {
-          return filesData.filter(file => file?.markedForDeleting !== true)
+          return filesData.filter((file) => file?.markedForDeleting !== true);
         }
 
         // Update state after all mutations are complete
@@ -303,7 +316,7 @@ export default function PrimaryImages() {
           size: file.size,
           activity: activityId,
         }));
-        setFiles(updatedFiles => updatedFiles.filter(f => f !== null));
+        setFiles((updatedFiles) => updatedFiles.filter((f) => f !== null));
       } catch (error) {
         // Handle errors if needed
         console.error("Error saving images:", error);
@@ -312,69 +325,84 @@ export default function PrimaryImages() {
   }
 
   async function handleAppend(acceptedFiles) {
-    setFiles(files => [...files, ...acceptedFiles.map((file, index) => {
+    setFiles((files) => [
+      ...files,
+      ...acceptedFiles.map((file, index) => {
         return Object.assign(file, {
           preview: URL.createObjectURL(file),
-        })
+        });
       }),
     ]);
   }
 
   return (
-    <Container sx={{
-      py: {xs: 6, md: 3},
-      backgroundColor: "#ffffff",
-      borderRadius: "16px",
-      display: "flex",
-      flexDirection: "column",
-    }}>
+    <Container
+      sx={{
+        py: { xs: 6, md: 3 },
+        backgroundColor: "#ffffff",
+        borderRadius: "16px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Box>
-        <Typography sx={{
-          fontFamily: "Montserrat",
-          fontSize: 20,
-          fontStyle: "normal",
-          fontWeight: "600",
-          lineHeight: 1.4, /* 140% */
-          letterSpacing: 0.2,
-          height: 50,
-          display: "flex",
-          pb: 5,
-        }}>Upload
+        <Typography
+          sx={{
+            fontFamily: "Montserrat",
+            fontSize: 20,
+            fontStyle: "normal",
+            fontWeight: "600",
+            lineHeight: 1.4 /* 140% */,
+            letterSpacing: 0.2,
+            height: 50,
+            display: "flex",
+            pb: 5,
+          }}
+        >
+          Upload
         </Typography>
       </Box>
-      <ImageInput files={files} handleAppend={handleAppend} multiple={true} sx={{
-        backgroundColor: "#DEE2E6",
-        height: 110,
-        borderRadius: "16px",
-      }}>
-      </ImageInput>
+      <ImageInput
+        files={files}
+        handleAppend={handleAppend}
+        multiple={true}
+        sx={{
+          backgroundColor: "#DEE2E6",
+          height: 110,
+          borderRadius: "16px",
+        }}
+      ></ImageInput>
+      {files.length !== 0 && <FilesTable files={files} setFiles={setFiles} />}
       {files.length !== 0 && (
-        <FilesTable files={files} setFiles={setFiles} />
-      )}
-      {files.length !== 0 && (
-        <Button onClick={SaveButtonHandler} color="green" sx={{
-          width: "20%",
-          height: 37,
-          py: 11,
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: "7px",
-          background: "#00A551",
-          alignSelf: "end",
-          py: 3,
-        }}>
-          <Typography sx={{
-            color: "#FFF",
-            textAlign: "center",
-            fontFamily: "Manrope",
-            fontStyle: "normal",
-            fontWeight: 800,
-            lineHeight: "131.5%", /* 13.786px */
-          }}>
+        <Button
+          onClick={SaveButtonHandler}
+          color="green"
+          sx={{
+            width: "20%",
+            height: 37,
+            py: 11,
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "7px",
+            background: "#00A551",
+            alignSelf: "end",
+            py: 3,
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#FFF",
+              textAlign: "center",
+              fontFamily: "Manrope",
+              fontStyle: "normal",
+              fontWeight: 800,
+              lineHeight: "131.5%" /* 13.786px */,
+            }}
+          >
             Save
           </Typography>
         </Button>
       )}
     </Container>
-  )
+  );
 }
