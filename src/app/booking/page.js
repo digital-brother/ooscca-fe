b2be678476dfc8c2c948dec46ada7d68f86b2a1c
Slava Loievskyi <loievskyi.slava@gmail.com>
2024-02-13 11:34:35 +0200

    Removed wrong pushed

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,12 +1,10 @@
 import Benefits from "./Benefits";
 import Intro from "./Intro";
-import Card from "./Card";
 
 
 export default function Booking() {
   return (
     <>
-      <Card/>
       <Intro/>
       <Benefits/>
     </>
