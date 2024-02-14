b37693f611012dfabad5f76242c77bbbd7a2fdd8
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-14 17:52:52 +0200

    Reformat

--- src/app/(homepage)/HomepageTabs.js
+++ src/app/(homepage)/HomepageTabs.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Box from "@mui/material/Box";
-import {Button, Container, styled} from "@mui/material";
+import { Button, Container, styled } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
@@ -11,8 +11,7 @@ import Tab from "@mui/material/Tab";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import SchoolStats from "@/app/(homepage)/SchoolStats";
-import Image from 'next/image';
-
+import Image from "next/image";
 
 const TABS_CONTENT = {
   classBirthdayCalendar: {
@@ -20,17 +19,18 @@ const TABS_CONTENT = {
     header: "Birthdays matter. Of course they matter!",
     subheader: "Celebrate Every Milestone",
     button: "Unlock birthday calendar",
-    description: "Birthdays parties, presents, cakes, pictures, cards, friends invite lists, plans, surprises, upsets —they all matter. To the child and the parents it is a very important personal event."
+    description:
+      "Birthdays parties, presents, cakes, pictures, cards, friends invite lists, plans, surprises, upsets —they all matter. To the child and the parents it is a very important personal event.",
   },
   whoseWho: {
     image: "/school2.png",
     header: "Whose that again?",
     subheader: "Build meaningful connections",
     button: "Remembering made easy",
-    description: "Avoid the awkward conversations about whose who when you meet at the gates or the next gathering. Your one place to get all your links, names and classmates in order!"
+    description:
+      "Avoid the awkward conversations about whose who when you meet at the gates or the next gathering. Your one place to get all your links, names and classmates in order!",
   },
-}
-
+};
 
 const CustomTab = styled(Tab)(({ theme }) => ({
   textTransform: "none",
@@ -54,26 +54,21 @@ const CustomTab = styled(Tab)(({ theme }) => ({
   fontSize: 16,
 }));
 
-
 function HomepageTabsHeader({ sx }) {
   return (
     <Box {...sx}>
-      <Typography variant="subheading">
-        Effortless tracking of every important detail
-      </Typography>
+      <Typography variant="subheading">Effortless tracking of every important detail</Typography>
       <Typography mt={2} variant="h2">
         Your central hub for key school details
       </Typography>
       <Typography mt={2} variant="body1" color="text.secondary">
-        You shouldn’t have your work cut out finding and syncing holidays,
-        birthdays and contacts. Once you&apos;re on board, you&apos;ll have access to
-        crucial information like school holiday schedules, classmates&apos;
-        birthdays, and parents&apos; contact details at your fingertips.
+        You shouldn’t have your work cut out finding and syncing holidays, birthdays and contacts. Once you&apos;re on
+        board, you&apos;ll have access to crucial information like school holiday schedules, classmates&apos; birthdays,
+        and parents&apos; contact details at your fingertips.
       </Typography>
     </Box>
   );
 }
-
 
 function ContentPanel({ type }) {
   const data = TABS_CONTENT[type];
@@ -82,15 +77,15 @@ function ContentPanel({ type }) {
     <Box
       sx={{
         display: "flex",
-        flexDirection: {xs: "column", md: "row"},
-        alignItems: {xs: "center", md: "start"},
-        textAlign: {xs: 'center', md: 'left'},
+        flexDirection: { xs: "column", md: "row" },
+        alignItems: { xs: "center", md: "stretch" },
+        textAlign: { xs: "center", md: "left" },
         gap: 3,
-        maxWidth: {xs: 500, md: "none"},
+        maxWidth: { xs: 500, md: "none" },
         mx: "auto",
       }}
     >
-      <Box sx={{flex: 1}}>
+      <Box sx={{ flex: 1 }}>
         <Typography mb={2} variant="subheading">
           {data.subheader}
         </Typography>
@@ -100,24 +95,24 @@ function ContentPanel({ type }) {
         <Typography mb={6.7} variant="body1" color="text.secondary">
           {data.description}
         </Typography>
-        <Button variant="outlined" color="orange" size="large" >
+        <Button variant="outlined" color="orange" size="large">
           {data.button}
         </Button>
       </Box>
       <Box
         sx={{
           flex: 1,
-          width: '100%',
-          mt: {xs: 3, md: 0},
+          width: "100%",
+          mt: { xs: 3, md: 0 },
         }}
-        >
+      >
         <Image
           src={data.image}
           alt="School"
           width="0"
           height="0"
           sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
+          style={{ width: "100%", height: "auto" }}
         />
       </Box>
     </Box>
@@ -145,18 +140,9 @@ function Tabs(props) {
             sx={{ border: "1px #CED4DA solid", borderRadius: 2 }}
             orientation={isSmallScreen ? "vertical" : "horizontal"}
           >
-            <CustomTab
-              label="OOS calendar"
-              value="oosCalendar"
-            />
-            <CustomTab
-              label="Class birthday calendar"
-              value="classBirthdayCalendar"
-            />
-            <CustomTab
-              label="Whose who"
-              value="whoseWho"
-            />
+            <CustomTab label="OOS calendar" value="oosCalendar" />
+            <CustomTab label="Class birthday calendar" value="classBirthdayCalendar" />
+            <CustomTab label="Whose who" value="whoseWho" />
           </TabList>
         </Box>
         <Box sx={{ p: 0, pt: { xs: 4, md: 10 } }}>
@@ -178,9 +164,7 @@ function Tabs(props) {
 export default function HomepageTabs() {
   return (
     <Container sx={{ pt: { xs: 6, md: 10 } }}>
-      <HomepageTabsHeader
-        sx={{ maxWidth: 730, textAlign: "center", mx: "auto" }}
-      />
+      <HomepageTabsHeader sx={{ maxWidth: 730, textAlign: "center", mx: "auto" }} />
       <Tabs sx={{ mt: 5 }} />
     </Container>
   );
