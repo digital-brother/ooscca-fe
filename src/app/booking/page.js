f99c2c6010db85a75aac75041d74aa171aa149ef
Slava Loievskyi <loievskyi.slava@gmail.com>
2024-02-14 14:55:54 +0200

    Added BookingCardsCalendar (Title, DaySwitcher)

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,6 +1,12 @@
+import Card from "./Card"
+import BookingCardsCalendar from "./BookingCardsCalendar"
+
+
 export default function Booking() {
   return (
     <>
+      <Card />
+      <BookingCardsCalendar />
     </>
   )
 }
