9410cc0fed63377d285ea738138f96a9f27cfa5a
alexandr <80922850741@ukr.net>
2024-02-19 10:03:09 +0200

    add logo section

--- src/app/activities/[activityId]/detail/sections/LogoSection.js
+++ src/app/activities/[activityId]/detail/sections/LogoSection.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 import React from "react";
 import {useParams} from "next/navigation";
 import {useQuery} from "react-query";
-import {getActivity, getActivityImagesSecondary} from "@/app/activities/[activityId]/detail/api.mjs";
+import {getActivity} from "@/app/activities/[activityId]/detail/api.mjs";
 
 export default function ProviderLogo() {
   const activityId = useParams().activityId;
@@ -14,12 +14,11 @@ export default function ProviderLogo() {
   const imageSrc = activity && activity.providerLogo ? activity.providerLogo : "/default_logo.png";
 
   return (
-    <Container sx={{ py: { xs: 6, md: 10 } }}>
+    <Container sx={{ py: { xs: 5, md: 8 } }}>
       <Box
         sx={{
         display: "flex",
-        justifyContent: "center",
-        mx: 2,
+        justifyContent: { xs: "center", lg: "left" },
       }}>
         <Image
           src={imageSrc}
@@ -27,7 +26,7 @@ export default function ProviderLogo() {
           width="0"
           height="0"
           sizes="100vw"
-          style={{ width: "100%", height: "auto", maxWidth: 500 }}
+          style={{ width: "100%", height: "auto", maxWidth: 540 }}
         />
       </Box>
     </Container>
