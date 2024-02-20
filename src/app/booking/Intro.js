af2ec0a7b143cefa04c24bbbba86c6d1c7c45965
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-21 00:57:05 +0200

    Add IntroText

--- src/app/booking/Intro.js
+++ src/app/booking/Intro.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import bookingIntroImage from "/public/booking-intro.png";
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 export default function Intro() {
   return (
@@ -8,7 +8,6 @@ export default function Intro() {
       <Box
         sx={{
           height: { xs: 200, sm: 225, md: 275, lg: 300, xl: 325 },
-          border: 1,
           overflow: "hidden",
           position: "relative",
         }}
@@ -17,9 +16,23 @@ export default function Intro() {
           src={bookingIntroImage}
           alt="Booking intro"
           fill
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: "cover", objectPosition: "bottom" }}
         />
       </Box>
+      <Container>
+        <Box sx={{ maxWidth: 800, textAlign: "center", mx: "auto", mt: 3, mb: 10 }}>
+          <Typography variant="h6" color="orange.main">
+            Keeping it simple
+          </Typography>
+          <Typography variant="h3" mt={2}>
+            Enjoy the ease of booking activities without the text and email tennis
+          </Typography>
+          <Typography color="grey.500" mt={2}>
+            We know when kids are going to be Out Of School so why not plan by calendar instead of activities. It&apos;s
+            is simpler, easier and more intuitive.
+          </Typography>
+        </Box>
+      </Container>
     </>
   );
 }
