4a149897559b7e5235877f8316cc855bc82caa37
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-02-22 01:20:27 +0200

    Extract PickerDate

--- src/app/booking/ActivitiesCalendar.js
+++ src/app/booking/ActivitiesCalendar.js
@@ -1,11 +1,39 @@
 "use client";
 
-import { Button, IconButton, Typography } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { IconButton, Typography } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import dayjs from "dayjs";
 import { useState } from "react";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+function PickerDate({ date, setSelectedDate, isSelectedDate }) {
+  return (
+    <Box sx={{ pb: 2, borderBottom: "3px solid", borderColor: isSelectedDate ? "grey.900" : "transparent" }}>
+      <Box
+        onClick={() => setSelectedDate(date)}
+        sx={{
+          textAlign: "center",
+          display: "block",
+          bgcolor: isSelectedDate ? "grey.100" : "transparent",
+          "&:hover": {
+            bgcolor: "grey.200",
+          },
+          "&:active": {
+            bgcolor: "grey.300",
+          },
+          cursor: "pointer",
+          borderRadius: 2,
+          py: 1.5,
+          width: 45,
+        }}
+      >
+        <Typography variant="h5">{date.format("DD")}</Typography>
+        <Typography sx={{ fontWeight: 700 }}>{date.format("ddd")}</Typography>
+      </Box>
+    </Box>
+  );
+}
 
 export default function ActivitiesCalendar() {
   const today = dayjs();
@@ -13,7 +41,7 @@ export default function ActivitiesCalendar() {
   const [selectedDate, setSelectedDate] = useState(today);
 
   let monday;
-  if (dayOfWeek === 0 && dayOfWeek === 6) {
+  if (dayOfWeek === 0 || dayOfWeek === 6) {
     monday = selectedDate.startOf("week").add(1, "day").add(1, "week");
   } else {
     monday = selectedDate.startOf("week").add(1, "day");
@@ -37,38 +65,7 @@ export default function ActivitiesCalendar() {
         </IconButton>
         {weekDates.map((date, index) => {
           const isSelectedDate = date.isSame(selectedDate, "day");
-          console.log("========");
-          console.log(date);
-          console.log(selectedDate);
-          console.log(isSelectedDate);
-          return (
-            <Box
-              key={index}
-              sx={{ pb: 2, borderBottom: "3px solid", borderColor: isSelectedDate ? "grey.900" : "transparent" }}
-            >
-              <Box
-                onClick={() => setSelectedDate(date)}
-                sx={{
-                  textAlign: "center",
-                  display: "block",
-                  bgcolor: isSelectedDate ? "grey.100" : "transparent",
-                  "&:hover": {
-                    bgcolor: "grey.200",
-                  },
-                  "&:active": {
-                    bgcolor: "grey.300",
-                  },
-                  cursor: "pointer",
-                  borderRadius: 2,
-                  py: 1.5,
-                  width: 45,
-                }}
-              >
-                <Typography variant="h5">{date.format("DD")}</Typography>
-                <Typography sx={{ fontWeight: 700 }}>{date.format("ddd")}</Typography>
-              </Box>
-            </Box>
-          );
+          return <PickerDate key={index} {...{ date, setSelectedDate, isSelectedDate }} />;
         })}
         <IconButton onClick={() => setSelectedDate(selectedDate.add(1, "week"))}>
           <ArrowForwardIosIcon />
