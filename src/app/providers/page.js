60aea327a675ffe68a64ffa5b2798409f47ffe8f
terror53421 <urarubanenko@gmail.com>
2024-04-24 09:25:36 +0300

    fix page.js

--- src/app/providers/page.js
+++ src/app/providers/page.js
@@ -1,13 +1,11 @@
 import ProvidersSignUp from "@/app/providers/ProvidersSignUp";
 import ClientSchools from "@/app/(homepage)/ClientSchools";
-import ProviderDemoHeader from "@/app/providers/DemoProviders";
 
 export default function ProviderPage  () {
   return (
     <>
       <ProvidersSignUp/>
       <ClientSchools/>
-      <ProviderDemoHeader/>
     </>
   )
 }
