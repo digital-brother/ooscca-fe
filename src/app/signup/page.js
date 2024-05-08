00d8e314592ce81ddd653fc21807521c1f7702a9
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-05-08 16:45:02 +0300

    Center SignUp form on the page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,3 +1,9 @@
+import { Box } from "@mui/material";
+
 export default function SignUp() {
-  return <>SignUp</>
+  return (
+    <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Box sx={{ border: 1 }}>Signup</Box>
+    </Box>
+  );
 }
