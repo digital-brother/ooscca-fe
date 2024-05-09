3713d7071a400d3ad8a2d67184205a921eceda41
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-05-09 21:43:01 +0300

    Add formik

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
 import { Box, Button, Checkbox, Container, IconButton, TextField, Typography } from "@mui/material";
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded";
 import Image from "next/image";
 import Link from "../(homepage)/components/Link";
+import { Form, Formik } from "formik";
 
 export default function SignUp() {
   return (
@@ -17,27 +18,33 @@ export default function SignUp() {
             <HighlightOffRoundedIcon sx={{ color: "common.black", fontSize: 28 }} />
           </IconButton>
         </Box>
-        <Typography variant="h5" sx={{ mt: 6 }}>
-          Sign in or create an account
-        </Typography>
-        <Typography sx={{ mt: 1.5 }}>
-          Enjoy smoother planning, minimises personal and work scheduling conflicts, and maximises healthy family time.
-        </Typography>
-        <TextField label="Email Address" type="email" fullWidth required sx={{ mt: 3 }} />
-        <TextField label="Password" type="password" fullWidth required sx={{ mt: 1.5 }} />
-        <TextField label="Re-enter password" type="password" fullWidth required sx={{ mt: 1.5 }} />
+        <Formik>
+          <Form>
+            <Typography variant="h5" sx={{ mt: 6 }}>
+              Sign in or create an account
+            </Typography>
+            <Typography sx={{ mt: 1.5 }}>
+              Enjoy smoother planning, minimises personal and work scheduling conflicts, and maximises healthy family
+              time.
+            </Typography>
+            <TextField label="Email Address" type="email" fullWidth required sx={{ mt: 3 }} />
+            <TextField label="Password" type="password" fullWidth required sx={{ mt: 1.5 }} />
+            <TextField label="Re-enter password" type="password" fullWidth required sx={{ mt: 1.5 }} />
 
-        {/* To be refactored to formik staff */}
-        <Box sx={{ display: "flex", alignItems: "center", justifyContent: { xs: "center", md: "left" }, mt: 6 }}>
-          <Checkbox size="small" sx={{ ml: -1 }} />
-          <Typography variant="body2" color="text.secondary">
-            By signing up you accept our <Link href="#">Terms and Conditions</Link> and <Link href="#">Privacy Policy</Link>
-          </Typography>
-        </Box>
+            {/* To be refactored to formik staff */}
+            <Box sx={{ display: "flex", alignItems: "center", justifyContent: { xs: "center", md: "left" }, mt: 6 }}>
+              <Checkbox size="small" sx={{ ml: -1 }} />
+              <Typography variant="body2" color="text.secondary">
+                By signing up you accept our <Link href="#">Terms and Conditions</Link> and{" "}
+                <Link href="#">Privacy Policy</Link>
+              </Typography>
+            </Box>
 
-        <Button variant="contained" color="grey" fullWidth sx={{ mt: 1 }}>
-          Sign up with email
-        </Button>
+            <Button variant="contained" color="grey" fullWidth sx={{ mt: 1 }}>
+              Sign up with email
+            </Button>
+          </Form>
+        </Formik>
       </Box>
     </Container>
   );
