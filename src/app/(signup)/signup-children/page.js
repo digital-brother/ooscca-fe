3228383b650725290bbd8853946049e6e02c7dfc
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-05-11 21:44:08 +0300

    Add school year

--- src/app/(signup)/signup-children/page.js
+++ src/app/(signup)/signup-children/page.js
@@ -5,6 +5,7 @@ import {
   FormikSelect,
   FormikTextField,
 } from "@/app/activities/[activityId]/edit/components/formikFields";
+import { SmFlex } from "@/app/activities/[activityId]/edit/components/responsiveFlexes";
 import { getSchools } from "@/app/api.mjs";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded";
@@ -17,6 +18,9 @@ import * as Yup from "yup";
 
 export default function SignUpChildren() {
   const { data: schools } = useQuery("schools", getSchools);
+
+  const classesYears = Array.from({ length: 8 }, (v, i) => `Year ${i + 1}`);
+  classesYears.unshift("Reception");
 
   return (
     <Container sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", py: 10 }}>
@@ -31,11 +35,11 @@ export default function SignUpChildren() {
           Children’s details
         </Typography>
         <Typography sx={{ mt: 1.5, textAlign: "center" }}>
-          Add your child(ren)’s details here and we’ll do the rest
+          Add your child(ren)&apos;s details here and we&apos;ll do the rest
         </Typography>
         <Typography sx={{ fontWeight: 700, mt: 6 }}>Child 1</Typography>
         <Formik
-          initialValues={{ firstName: "", lastName: "", displayName: "", birthDate: null, school: "" }}
+          initialValues={{ firstName: "", lastName: "", displayName: "", birthDate: null, classYear: "", school: "" }}
           onSubmit={(values) => console.log(values)}
           validationSchema={Yup.object({
             firstName: Yup.string()
@@ -74,21 +78,32 @@ export default function SignUpChildren() {
               .typeError("Invalid date")
               .min(1990)
               .max(dayjs(), `Date of birth must be before ${dayjs().format("MM/DD/YYYY")}`),
-            school: Yup.number().label("School name").required(),
+            school: Yup.number().label("School").required(),
+            classYear: Yup.string().label("Class/year").required(),
           })}
         >
           <Form>
             <FormikTextField name="firstName" label="First name" fullWidth sx={{ mt: 1.5 }} />
             <FormikTextField name="lastName" label="Last name" fullWidth sx={{ mt: 1.5 }} />
             <FormikTextField name="displayName" label="Display/nick name" fullWidth sx={{ mt: 1.5 }} />
-            <FormikDateField name="birthDate" label="Date of birth" fullWidth sx={{ mt: 1.5 }} disableFuture />
-            <FormikSelect name="school" label="School name" fullwidth sx={{ mt: 1.5 }}>
-              {(schools || []).map((school) => (
-                <MenuItem key={school.id} value={school.id}>
-                  {school.name}
+            <SmFlex sx={{ gap: 1.5, mt: 1.5 }}>
+              <FormikDateField name="birthDate" label="Date of birth" fullWidth disableFuture />
+              <FormikSelect name="school" label="School" fullwidth>
+                {(schools || []).map((school) => (
+                  <MenuItem key={school.id} value={school.id}>
+                    {school.name}
+                  </MenuItem>
+                ))}
+              </FormikSelect>
+            </SmFlex>
+            <FormikSelect name="classYear" label="Class/year" fullwidth sx={{ mt: 1.5 }}>
+              {classesYears.map((classYear, index) => (
+                <MenuItem key={index} value={classYear}>
+                  {classYear}
                 </MenuItem>
               ))}
             </FormikSelect>
+
             <Button
               type="submit"
               variant="outlined"
