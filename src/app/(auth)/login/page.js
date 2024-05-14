877799b5be472128fa345e740792c36309ef3794
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-05-14 23:37:48 +0300

    Add Login page

---
+++ src/app/(auth)/login/page.js
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  FormikErrors,
+  FormikTextField,
+  createHandleSubmit,
+} from "@/app/activities/[activityId]/edit/components/formikFields";
+import { login } from "@/app/api.mjs";
+import { Button, Container, Typography } from "@mui/material";
+import { Form, Formik } from "formik";
+import { useMutation } from "react-query";
+import * as Yup from "yup";
+import { SignUpContainer } from "../signup/components/SignUpAccount";
+import { useSnackbar } from "notistack";
+
+export default function Login() {
+  const mutation = useMutation(login);
+    const { enqueueSnackbar } = useSnackbar();
+
+  async function handleSubmit(values, formikHelpers) {
+    const handle = createHandleSubmit({
+      mutation,
+      onSuccess: ({ key: token }) => {
+        localStorage.setItem("authToken", token);
+        enqueueSnackbar("Login successful", { variant: "success" });
+      },
+    });
+    handle(values, formikHelpers);
+  }
+
+  return (
+    <Container sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", py: 10 }}>
+      <SignUpContainer sx={{ textAlign: "center" }}>
+        <Typography variant="h5" sx={{ mt: 6 }}>
+          Log in
+        </Typography>
+        <Typography sx={{ mt: 1.5 }}>
+          Enjoy smoother planning, minimises personal and work scheduling conflicts, and maximises healthy family time.
+        </Typography>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={{ email: "", password: "" }}
+          validationSchema={Yup.object({
+            email: Yup.string().label("Email address").email("Invalid email address").required(),
+            password: Yup.string().label("Password").label("Password").required(),
+          })}
+        >
+          <Form>
+            <FormikTextField name="email" label="Email Address" fullWidth sx={{ mt: 3 }} />
+            <FormikTextField name="password" label="Password" type="password" fullWidth sx={{ mt: 1.5 }} />
+            <Button type="submit" variant="contained" color="green" fullWidth sx={{ mt: 6 }}>
+              Login
+            </Button>
+            <FormikErrors />
+          </Form>
+        </Formik>
+      </SignUpContainer>
+    </Container>
+  );
+}
