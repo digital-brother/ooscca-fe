4465aaa0416e966c3bc6cad744028d8fa786b2cd
Maryna Velychko <velycko.maryna@gmail.com>
2024-05-16 17:09:20 +0300

    Save current step in local storage

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,14 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SignUpAccount from "./components/SignUpAccount";
 import SignUpDetails from "./components/SignUpDetails";
 import SignUpEmailConfirmation from "./components/SignUpEmailConfirmation";
 import SignUpChildren from "./components/SignUpChildren";
 
 export default function SignUp() {
-  const [currentStep, setCurrentStep] = useState(0);
+  const [currentStep, setCurrentStep] = useState(() => {
+    const savedStep = localStorage.getItem('signup_current_step');
+    return savedStep ? parseInt(savedStep) : 0;
+  });
+
   const steps = [SignUpAccount, SignUpDetails, SignUpChildren, SignUpEmailConfirmation];
   const CurrentStep = steps[currentStep];
+
+  useEffect(() => {
+    localStorage.setItem('signup_current_step', currentStep);
+  }, [currentStep]);
+  
   return <CurrentStep goToNextStep={() => setCurrentStep((currentStep) => currentStep + 1)} />;
 }
