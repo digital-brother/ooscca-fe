d35208f9f9d4ccaa1ef172607e4108dd0a0b85ff
Maryna Velychko <velycko.maryna@gmail.com>
2024-05-20 12:29:30 +0300

    Refactoring

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -8,6 +8,8 @@ import SignUpEmailConfirmation from "./components/SignUpEmailConfirmation";
 import SignUpChildren from "./components/SignUpChildren";
 import { AUTH_TOKEN_NAME } from "@/app/api.mjs";
 
+const SIGNUP_CURRENT_STEP_KEY = 'signup_current_step';
+
 export default function SignUp() {
   const router = useRouter();
   const [currentStep, setCurrentStep] = useState(0);
@@ -17,17 +19,19 @@ export default function SignUp() {
     if (authToken) {
       router.push('/');
     }
-    const savedStep = localStorage.getItem('signup_current_step');
+    const savedStep = localStorage.getItem(SIGNUP_CURRENT_STEP_KEY);
     setCurrentStep(savedStep ? parseInt(savedStep) : 0);
   }, []);
 
-  useEffect(() => {
-    if (currentStep !== 0)
-      localStorage.setItem('signup_current_step', currentStep);
-  }, [currentStep]);
+  const goToNextStep = () => {
+    setCurrentStep((prevStep) => {
+      const nextStep = prevStep + 1;
+      localStorage.setItem(SIGNUP_CURRENT_STEP_KEY, nextStep);
+      return nextStep;
+    });
+  };
 
   const steps = [SignUpAccount, SignUpDetails, SignUpChildren, SignUpEmailConfirmation];
   const CurrentStep = steps[currentStep];
-  
-  return <CurrentStep goToNextStep={() => setCurrentStep((currentStep) => currentStep + 1)} />;
+  return <CurrentStep goToNextStep={goToNextStep} />;
 }
