c7ab7d987a5e4e96f1751387ee9771d4a9f22e3d
Maryna Velychko <velycko.maryna@gmail.com>
2024-06-25 22:20:01 +0300

    Fix sintax

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -8,10 +8,11 @@ export default function Contact() {
     <Container sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", py: 10 }}>
       <OssContainer sx={{ textAlign: "center", border: 1, borderRadius: 1.5, pb: 15 }}>
         <Typography variant="h5" sx={{ mt: 10 }}>
-            Contact us
+          Contact us
         </Typography>
         <Typography sx={{ mt: 1.5 }}>
-            We&apos;re in beta so you&apos;ll likely come across some glitches. We&apos;d love to hear from you with any questions or suggestions, so give us a holla at the <Link href="mailto:team@ooscca.com">team@ooscca.com</Link>.
+          We&apos;re in beta so you&apos;ll likely come across some glitches. We&apos;d love to hear from you with any
+          questions or suggestions, so give us a holla at the <Link href="mailto:team@ooscca.com">team@ooscca.com</Link>
         </Typography>
       </OssContainer>
     </Container>
